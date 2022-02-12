dabd9367b572267c13ed20541f41073f77352113
Andrei Kavaleuski <spyroid@gmail.com>
2022-02-12 17:36:56 +0100

    .

---
+++ src/codility/toptal2022/Main.kt
@@ -0,0 +1,55 @@
+package codility.toptal2022
+
+import kotlin.system.measureTimeMillis
+
+fun solve(input: List<String>): Int {
+    val arr1 = intArrayOf()
+    val arr2 = intArrayOf()
+    return solution2(arr1, arr2)
+}
+
+
+fun solution2(P: IntArray, S: IntArray): Int {
+    var freeSeats = 0
+    for (i in 0..P.lastIndex) if (S[i] - P[i] > 0) freeSeats += S[i] - P[i]
+    P.sort()
+    for (i in 0..P.lastIndex) {
+        if (P[i] <= freeSeats) {
+            freeSeats -= P[i]
+            P[i] = 0
+        } else break
+    }
+    return P.count { it > 0 }
+}
+
+fun solution3(A: IntArray): Int {
+    val initSum = A.sum()
+    var filters = 0
+    if (A.isEmpty()) return 0
+
+    do {
+        A.sortDescending()
+        A[0] /= 2
+        filters++
+    } while (A.sum() > initSum / 2)
+
+    return filters
+}
+
+
+fun solution1(message: String, K: Int): String {
+    var ids = -1
+    if (K >= message.length) return message
+    for (i in 0 until message.length - 1) {
+        if (!message[i].isWhitespace() && message[i + 1].isWhitespace()) ids = i + 1
+        if (i >= K) break
+    }
+    return message.substring(0, ids)
+}
+
+fun main() {
+    measureTimeMillis { print("⭐️ Result: ${solve(listOf())}") }
+        .also { time -> println(" in $time ms") }
+}
+
+class Main {}
