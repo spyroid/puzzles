30c86a7fd41065acd40d24100d9d074e2cf60cba
Andrei Kavaleuski <spyroid@gmail.com>
2022-04-19 00:39:48 +0200

    .

--- src/codility/toptal202204/Solution3.kt
+++ src/codility/toptal202204/Solution3.kt
@@ -3,32 +3,25 @@ package codility.toptal202204
 import runWithTime
 
 fun main() {
-    runWithTime { (solution(listOf(2, 1, 3).toIntArray(), 2)) }
+    runWithTime { (solution(listOf(5, 3, 6, 2, 5, 5, 2).toIntArray(), 4)) }
+//    runWithTime { (solution(listOf(2, 1, 3).toIntArray(), 2)) }
 //    runWithTime { (solution(listOf(0, 4, 3, -1).toIntArray(), 2)) }
 //    runWithTime { (solution(listOf(2, 1, 4).toIntArray(), 3)) }
 }
 
+// average = (A[i] + A[i+1] ... + A[j]) / (j - i + 1)
+// average * (j - i + 1) = A[i] + A[i+1]...+ A[j]
+// partial sums that is P[0] = 0 and P[i] = X[1] + ... + X[i]
 fun solution(A: IntArray, S: Int): Int {
-    val prefixes = mutableMapOf<Int, Int>()
-    prefixes[0] = 1
-    var result = 0
-    val P = IntArray(A.size + 1)
-    val Q = IntArray(A.size + 1)
+    val prefs = mutableMapOf<Long, Int>().also { it[0] = 1 }
+    val partialSums = LongArray(A.size + 1)
+    val Q = LongArray(A.size + 1)
 
     for (i in 1 until A.size + 1) {
-        P[i] = P[i - 1] + A[i - 1]
-        Q[i] = P[i] - S * i
-        if (!prefixes.containsKey(Q[i])) {
-            prefixes[Q[i]] = 1
-        } else {
-            var temp = prefixes[Q[i]]!!
-            temp += 1
-            prefixes[Q[i]] = temp
-        }
+        partialSums[i] = partialSums[i - 1] + A[i - 1]
+        Q[i] = partialSums[i] - S * i
+        prefs.merge(Q[i], 1) { prev, one -> prev + one }
     }
-    println(prefixes)
 
-    for (v in prefixes.values) result += v * (v - 1) / 2
-
-    return result
+    return prefs.values.sumOf { it * (it - 1) / 2 }
 }
