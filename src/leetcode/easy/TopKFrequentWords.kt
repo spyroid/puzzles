198f34708fd8b229101451a25bd966e6a55c3014
Andrei Kovalevsky <spyroid@gmail.com>
2022-10-19 15:54:10 +0200

    .

---
+++ src/leetcode/easy/TopKFrequentWords.kt
@@ -0,0 +1,18 @@
+package leetcode.easy
+
+import puzzle
+
+fun main() {
+    puzzle {
+        topKFrequent(listOf("i","love","leetcode","i","love","coding").toTypedArray(), 2)
+    }
+    puzzle {
+        topKFrequent(listOf("the","day","is","sunny","the","the","the","sunny","is","is").toTypedArray(), 4)
+    }
+}
+
+fun topKFrequent(words: Array<String>, k: Int): List<String> {
+    return words.asSequence().groupingBy { it }.eachCount().toList()
+        .sortedWith(compareByDescending<Pair<String, Int>> { it.second }.thenBy { it.first })
+        .take(k).map { it.first }
+}
