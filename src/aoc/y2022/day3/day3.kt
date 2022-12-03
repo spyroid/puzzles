160dfc7398da26f2a662f4c5780938200ecbdabe
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-03 09:49:58 +0100

    3

--- src/aoc/y2022/day3/day3.kt
+++ src/aoc/y2022/day3/day3.kt
@@ -14,14 +14,12 @@ private fun part1(input: List<String>): Int = input.asSequence()
     .map { it.chunked(it.length / 2) }
     .map { it[0].toSet() intersect it[1].toSet() }
     .flatMap { it }
-    .map { prioritize(it) }
-    .sum()
+    .sumOf { prioritize(it) }
 
 private fun part2(input: List<String>): Int = input.asSequence()
     .windowed(3, 3)
     .map { it[0].toSet() intersect it[1].toSet() intersect it[2].toSet() }
     .flatMap { it }
-    .map { prioritize(it) }
-    .sum()
+    .sumOf { prioritize(it) }
 
 private fun prioritize(c: Char) = if (c in 'a'..'z') c.code - 96 else c.code - 38
