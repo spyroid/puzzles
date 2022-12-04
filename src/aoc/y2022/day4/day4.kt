88e7213ec16f798a5b167434355b433ecef23dbf
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-04 10:49:00 +0100

    4

--- src/aoc/y2022/day4/day4.kt
+++ src/aoc/y2022/day4/day4.kt
@@ -10,13 +10,13 @@ fun main() {
     puzzle { part2(readLinesFrom("input.txt")) }
 }
 
-private fun asRanges(list: List<String>) = list.map { line ->
-    line.split(",").map { pair -> pair.split("-").let { IntRange(it[0].toInt(), it[1].toInt()) } }
-}
-
 private fun part1(input: List<String>) = asRanges(input).count { it[0] isFullyOverlapped it[1] }
 
 private fun part2(input: List<String>) = asRanges(input).count { it[0] isPartiallyOverlapped it[1] }
+
+private fun asRanges(list: List<String>) = list.map { line ->
+    line.split(",").map { pair -> pair.split("-").let { IntRange(it[0].toInt(), it[1].toInt()) } }
+}
 
 infix fun IntRange.isFullyOverlapped(other: IntRange): Boolean = (first in other && last in other) || (other.first in this && other.last in this)
 
