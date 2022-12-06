f34bcc66209e6b33d11cf14d21f4051b23a8edb5
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-06 06:51:50 +0100

    6

---
+++ src/aoc/y2022/day6/day6.kt
@@ -0,0 +1,18 @@
+package aoc.y2022.day6
+
+import puzzle
+
+fun main() {
+    puzzle { findMarker(readLinesFrom("test.txt").first(), 4) }
+    puzzle { findMarker(readLinesFrom("input.txt").first(), 4) }
+
+    puzzle { findMarker(readLinesFrom("test.txt").first(), 14) }
+    puzzle { findMarker(readLinesFrom("input.txt").first(), 14) }
+}
+
+private fun findMarker(input: String, size: Int): Int {
+    return input
+        .windowed(size = size, step = 1, partialWindows = false)
+        .mapIndexed { i, s -> (s.toSet().count() == size) to (size + i) }
+        .first { it.first }.second
+}
