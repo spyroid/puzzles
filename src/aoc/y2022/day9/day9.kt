ecd5572294b47be48682984b3b26adf23ef5b538
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-09 14:12:16 +0100

    9 with Point and Direction helpers

--- src/aoc/y2022/day9/day9.kt
+++ src/aoc/y2022/day9/day9.kt
@@ -10,19 +10,17 @@ fun main() {
     puzzle("2") { rope(linesFrom("input.txt"), 10) }
 }
 
-private fun rope(input: List<String>, knots: Int): Int {
-    return buildSet {
-        val points = MutableList(knots) { Point.zero }
-        input.forEach { s ->
-            val (dir, count) = s.split(" ").let { Pair(Direction.of(it[0]), it[1].toInt()) }
-            repeat(count) {
-                points[0] = points[0] + dir
-                for (i in 1 until points.size) {
-                    val dist = points[i - 1] - points[i]
-                    if (dist.chebyshev() > 1) points[i] = points[i] + dist.asDirection()
-                }
-                add(points.last())
+private fun rope(input: List<String>, knots: Int): Int = buildSet {
+    val points = MutableList(knots) { Point.zero }
+    input.forEach { s ->
+        val (dir, count) = s.split(" ").let { Pair(Direction.of(it[0]), it[1].toInt()) }
+        repeat(count) {
+            points[0] = points[0] + dir
+            for (i in 1 until points.size) {
+                val dist = points[i - 1] - points[i]
+                if (dist.chebyshev() > 1) points[i] = points[i] + dist.asDirection()
             }
+            add(points.last())
         }
-    }.size
-}
+    }
+}.size
