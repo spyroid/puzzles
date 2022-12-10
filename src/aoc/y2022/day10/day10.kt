307ebdc45de9451d0fde76c4eea85f886a435c4d
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-10 14:29:32 +0100

    10

--- src/aoc/y2022/day10/day10.kt
+++ src/aoc/y2022/day10/day10.kt
@@ -1,6 +1,8 @@
 package aoc.y2022.day10
 
+import fullBlock
 import puzzle
+import kotlin.math.abs
 
 fun main() {
     puzzle { crt(linesFrom("test.txt")) }
@@ -11,10 +13,10 @@ private fun crt(input: List<String>): Int {
     return sequence {
         input.forEach { s -> yield(0).also { if (s.startsWith("addx")) yield(s.drop(5).toInt()) } }
     }.foldIndexed(Pair(1, 0)) { i, acc, v ->
-        val strength = if (i + 1 == 20 || (i - 19) % 40 == 0) acc.first * (i + 1) else 0
+        val strength = if (abs(i - 19) % 40 == 0) acc.first * (i + 1) else 0
         Pair(acc.first + v, acc.second + strength).also {
             if (i % 40 == 0) println()
-            if (i % 40 + 1 in acc.first..acc.first + 2) print("#") else print(".")
+            if (i % 40 + 1 in acc.first..acc.first + 2) print(fullBlock) else print(".")
         }
     }.second.also { println() }
 }
