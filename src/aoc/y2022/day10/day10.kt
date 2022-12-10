531c71970df259fdda50ca8133f9a53a3808fe94
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-10 16:46:47 +0100

    10

--- src/aoc/y2022/day10/day10.kt
+++ src/aoc/y2022/day10/day10.kt
@@ -10,13 +10,14 @@ fun main() {
 }
 
 private fun crt(input: List<String>): Int {
-    return sequence {
-        input.forEach { s -> yield(0).also { if (s.startsWith("addx")) yield(s.drop(5).toInt()) } }
-    }.foldIndexed(Pair(1, 0)) { i, acc, v ->
-        val strength = if (abs(i - 19) % 40 == 0) acc.first * (i + 1) else 0
-        Pair(acc.first + v, acc.second + strength).also {
+    return input.flatMap { it.split(" ") }
+        .map { if (it.last().isDigit()) it.toInt() else 0 }
+        .scan(1, Int::plus)
+        .dropLast(1)
+        .mapIndexed { i, x ->
+            val strength = if (abs(i - 19) % 40 == 0) x * (i + 1) else 0
             if (i % 40 == 0) println()
-            if (i % 40 + 1 in acc.first..acc.first + 2) print(fullBlock) else print(".")
-        }
-    }.second.also { println() }
+            if (i % 40 + 1 in x..x + 2) print(fullBlock) else print(".")
+            strength
+        }.sumOf { it }.also { println() }
 }
