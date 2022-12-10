3796cc6bc0cc6416e050ec7707598b89613333e7
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-10 16:57:08 +0100

    10

--- src/aoc/y2022/day10/day10.kt
+++ src/aoc/y2022/day10/day10.kt
@@ -12,12 +12,11 @@ fun main() {
 private fun crt(input: List<String>): Int {
     return input.flatMap { it.split(" ") }
         .map { if (it.last().isDigit()) it.toInt() else 0 }
-        .scan(1, Int::plus)
-        .dropLast(1)
-        .mapIndexed { i, x ->
-            val strength = if (abs(i - 19) % 40 == 0) x * (i + 1) else 0
+        .scan(1, Int::plus).dropLast(1)
+        .onEachIndexed { i, x ->
             if (i % 40 == 0) println()
             if (i % 40 + 1 in x..x + 2) print(fullBlock) else print(".")
-            strength
-        }.sumOf { it }.also { println() }
+        }
+        .mapIndexed { i, x -> if (abs(i - 19) % 40 == 0) x * (i + 1) else 0 }
+        .sumOf { it }.also { println() }
 }
