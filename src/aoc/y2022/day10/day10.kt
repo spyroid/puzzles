a64c075621ea971d15e2b165a2db2fdc306dfae7
Andrei Kovalevsky <spyroid@gmail.com>
2022-12-10 11:19:39 +0100

    10

--- src/aoc/y2022/day10/day10.kt
+++ src/aoc/y2022/day10/day10.kt
@@ -3,36 +3,28 @@ package aoc.y2022.day10
 import puzzle
 
 fun main() {
-    puzzle("test 1") {
-        part1(linesFrom("test.txt"))
-    }
-    puzzle("1") {
-        part1(linesFrom("input.txt"))
-    }
+    puzzle("test 1") { crt(linesFrom("test.txt")) }
+    puzzle("1") { crt(linesFrom("input.txt")) }
 }
 
-private fun part1(input: List<String>): Int {
+private fun crt(input: List<String>): Int {
     var x = 1
-    var strength = 0
-    crt(input).forEachIndexed { i, pair ->
-        var cycle = i + 1
-        if (cycle == 20 || (cycle - 20) % 40 == 0) { strength += x * cycle }
-        var pos = i % 40
-        if (pos == 0) println()
-        if (pos + 1 in x..x+2) print("#") else print(".")
-        x += pair.second
-    }
-    println()
-
-    return strength
-}
-
-private fun crt(input: List<String>) = sequence {
-    input.forEach { s ->
-        s.split(" ").plus("0").let {
-            if (it[0] == "noop") yield(Pair(it[0], 0)) else {
-                yieldAll(listOf(Pair(it[0], 0), Pair(it[0], it[1].toInt())))
+    return sequence {
+        input.forEach {
+            it.split(" ").plus("").let { (op, v) ->
+                if (op == "noop") yield(0) else yieldAll(listOf(0, v.toInt()))
             }
         }
-    }
+    }.mapIndexed { i, v ->
+        var cycle = i + 1
+        val strength = if (cycle == 20 || (cycle - 20) % 40 == 0) {
+            x * cycle
+        } else 0
+
+        var pos = i % 40
+        if (pos == 0) println()
+        if (pos + 1 in x..x + 2) print("#") else print(".")
+
+        strength.also { x += v }
+    }.sum().also { println() }
 }
