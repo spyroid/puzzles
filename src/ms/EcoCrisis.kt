453a8c80914a7b6533ed6233fc3e8900ff989b3b
Andrei Kovalevsky <spyroid@gmail.com>
2023-05-29 16:08:09 +0200

    MS

--- src/ms/EcoCrisis.kt
+++ src/ms/EcoCrisis.kt
@@ -17,20 +17,20 @@ private fun solution(s: String): Int {
     val list = s.toMutableList()
 
     list.windowed(3).onEachIndexed { i, sub ->
-        if (sub == listOf('H', '-', 'H')) if (!list.hasIt(i) && !list.hasIt(i + 2)) list[i + 1] = '*'
+        if (sub == listOf('H', '-', 'H')) if (!list.hasAround(i) && !list.hasAround(i + 2)) list[i + 1] = '*'
     }
     list.windowed(2).onEachIndexed { i, sub ->
-        if (sub == listOf('-', 'H') && !list.hasIt(i + 1)) list[i] = '*'
-        if (sub == listOf('H', '-') && !list.hasIt(i)) list[i + 1] = '*'
+        if (sub == listOf('-', 'H') && !list.hasAround(i + 1)) list[i] = '*'
+        if (sub == listOf('H', '-') && !list.hasAround(i)) list[i + 1] = '*'
     }
     list.windowed(2).onEachIndexed { i, sub ->
-        if (sub == listOf('H', 'H') && (!list.hasIt(i) || !list.hasIt(i + 1))) return -1
+        if (sub == listOf('H', 'H') && (!list.hasAround(i) || !list.hasAround(i + 1))) return -1
     }
     val count = list.count { it == '*' }
     return if (count == 0) -1 else count
 }
 
-private fun List<Char>.hasIt(idx: Int): Boolean {
+private fun List<Char>.hasAround(idx: Int): Boolean {
     if (idx < 0 || this[idx] != 'H') return false
     if (this.getOrElse(idx - 1) { _ -> '.' } == '*') return true
     return this.getOrElse(idx + 1) { _ -> '.' } == '*'
