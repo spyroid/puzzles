40e4fa4ef82da82b8e17f54a640f9aa5c67b30f4
Andrei Kovalevsky <spyroid@gmail.com>
2023-07-10 01:58:06 +0200

    10

---
+++ src/aoc/y2020/day10/solution.kt
@@ -0,0 +1,33 @@
+package aoc.y2020.day10
+
+import gears.puzzle
+import gears.toInts
+
+private fun main() {
+    puzzle { part1(linesFrom("test.txt").toInts()) }
+    puzzle { part1(linesFrom("input.txt").toInts()) }
+    puzzle { part2(linesFrom("test.txt").toInts()) }
+    puzzle { part2(linesFrom("input.txt").toInts()) }
+}
+
+private fun part1(input: List<Int>): Int {
+    val s = input.sorted()
+    val all = listOf(0).plus(s).plus(s.last() + 3)
+    return all.zipWithNext()
+        .map { it.second - it.first }
+        .groupingBy { it }
+        .eachCount()
+        .let { it[1]!! * it[3]!! }
+}
+
+private fun part2(input: List<Int>): Long {
+    val k = longArrayOf(1, 0, 0, 0)
+    input.sorted().fold(0) { a, b ->
+        val d = b - a
+        k.copyInto(k, d, 0, k.size - d)
+        k.fill(0, 0, d)
+        k[0] += k.sum()
+        b
+    }
+    return k[0]
+}
--- src/gears/Utils.kt
+++ src/gears/Utils.kt
@@ -4,7 +4,8 @@ import java.io.File
 
 const val baseDir = "src."
 
-private fun localDir(klass: Any) = File((baseDir + klass.javaClass.packageName).replace(".", File.separatorChar.toString()))
+private fun localDir(klass: Any) =
+    File((baseDir + klass.javaClass.packageName).replace(".", File.separatorChar.toString()))
 
 fun readLocal(klass: Any, fileName: String) = File(localDir(klass), fileName).readLines()
 
@@ -13,7 +14,10 @@ fun readRawInput(name: String) = File("src", "$name.txt").readText()
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 fun List<String>.toIntSeq() = this.asSequence().map { it.toInt() }
+fun List<String>.toLongs() = this.map { it.toLong() }
+fun List<String>.toInts() = this.map { it.toInt() }
 
-fun readIntsAsSeq(name: String): Sequence<Int> = readInput(name).joinToString().split(",").map { it.toInt() }.asSequence()
+fun readIntsAsSeq(name: String): Sequence<Int> =
+    readInput(name).joinToString().split(",").map { it.toInt() }.asSequence()
 
 fun readToIntSeq(name: String) = readInput(name).toIntSeq()
