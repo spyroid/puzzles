16057736f34ef6ae24d0697f5ef8a50aadd58882
Andrei Kovalevsky <spyroid@gmail.com>
2023-07-13 01:16:17 +0200

    16

---
+++ src/aoc/y2020/day16/solution.kt
@@ -0,0 +1,11 @@
+package aoc.y2020.day16
+
+import gears.puzzle
+
+private fun main() {
+    puzzle { part1(linesFrom("test.txt")) }
+}
+
+private fun part1(input: List<String>): Int {
+    return 0
+}
