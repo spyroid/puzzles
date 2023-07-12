a15e45a9dabe5e0dfa42baa905d528417995eeab
Andrei Kovalevsky <spyroid@gmail.com>
2023-07-13 01:04:25 +0200

    15

---
+++ src/aoc/y2020/day15/solution.kt
@@ -0,0 +1,29 @@
+package aoc.y2020.day15
+
+import gears.puzzle
+
+private fun main() {
+    val test = listOf(0, 3, 6)
+    val input = listOf(13, 16, 0, 12, 15, 1)
+
+    puzzle { part1(test, 2020) }
+    puzzle { part1(input, 2020) }
+    puzzle { part1(test, 30000000) }
+    puzzle { part1(input, 30000000) }
+}
+
+private fun part1(input: List<Int>, iterations: Long): Int {
+    val history = mutableMapOf<Int, ArrayDeque<Int>>()
+    input.forEachIndexed { i, v -> history.getOrPut(v) { ArrayDeque() }.add(i) }
+
+    var current = input.last()
+    var iter = input.size
+
+    while (iter < iterations) {
+        val pr = history[current]
+        current = if (pr == null || pr.size < 2) 0 else pr.last() - pr[pr.lastIndex - 1]
+        history.getOrPut(current) { ArrayDeque() }.add(iter)
+        iter += 1
+    }
+    return current
+}
