0e84a135ca6e37b0dbb59c5f2d4f98b798f84b5f
Andrei Kovalevsky <spyroid@gmail.com>
2023-07-26 20:50:26 +0200

    8

---
+++ src/aoc/y2015/day8/solution.kt
@@ -0,0 +1,27 @@
+package aoc.y2015.day8
+
+import gears.puzzle
+
+private fun main() {
+    puzzle { part1(linesFrom("test.txt")) }
+    puzzle { part1(linesFrom("input.txt")) }
+    puzzle { part2(linesFrom("input.txt")) }
+}
+
+private fun part1(lines: List<String>): Int {
+    return lines.sumOf { line ->
+        line.length - line
+            .replace("\\\\", "^")
+            .replace(Regex("[^\\\\]\\\\x\\w{2}")) { "${it.value[0]}_" }
+            .replace(Regex("[^\\\\]\\\\x\\w{2}")) { "${it.value[0]}_" }
+            .replace("\\\"", "\"")
+            .replace("^", "\\")
+            .drop(1).dropLast(1).length
+    }
+}
+
+private fun part2(lines: List<String>): Int {
+    return lines.sumOf { line ->
+        line.sumOf { c -> if (c == '\\' || c == '\"') 2 else 1 as Int } + 2 - line.length
+    }
+}
