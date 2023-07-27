02c551116de76453b1c30560f90b72ddc14ed391
Andrei Kovalevsky <spyroid@gmail.com>
2023-07-27 23:42:19 +0200

    12

---
+++ src/aoc/y2015/day12/solution.kt
@@ -0,0 +1,27 @@
+package aoc.y2015.day12
+
+import gears.puzzle
+
+private fun main() {
+    puzzle {
+        part1(linesFrom("input.txt").first())
+    }
+    puzzle {
+        part2(linesFrom("input.txt").first())
+    }
+}
+
+private fun part1(json: String) = json.replace("[^0-9-]".toRegex(), " ")
+    .split("\\s+".toRegex())
+    .filter { it.isNotBlank() }
+    .sumOf { it.toInt() }
+
+private val re = "\\{[^{}]*\\}".toRegex()
+private fun part2(json: String): String {
+    return if (re.containsMatchIn(json)) {
+        part2(re.replace(json) { m -> if (m.value.contains(":\"red")) "0" else part1(m.value).toString() })
+    } else {
+        part1(json).toString()
+    }
+}
+
