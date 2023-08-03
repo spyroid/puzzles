0f26c9ac2dbe346d6264af56c2d70462bc647e3a
Andrei Kovalevsky <spyroid@gmail.com>
2023-08-03 19:47:56 +0200

    15

--- src/aoc/y2015/day15/solution.kt
+++ src/aoc/y2015/day15/solution.kt
@@ -3,39 +3,30 @@ package aoc.y2015.day15
 import gears.puzzle
 
 private fun main() {
-    puzzle {
-        part1(linesFrom("test.txt"))
-    }
+    puzzle { recipe() }
+    puzzle { recipe(true) }
 }
 
-private fun part1(lines: List<String>): Int {
-    val cap = listOf(-1, 2)
-    val dur = listOf(-2, 3)
-    val fl = listOf(6, -2)
-    val tex = listOf(3, -1)
-
+private fun recipe(withCalories: Boolean = false): Int {
     var res = 0
 
-    fun gen(total: Int): Sequence<List<Int>> {
-        val a = ArrayDeque(IntArray(total).also { it[0] = 10 }.toList())
-        return sequence {
-            repeat(total) { yield(a).also { a.shift() } }
-            var j = 1
-            for (i in 9 downTo 5) {
-                a[0] = i
-                var ii = j++ % total
-                if (ii == 0) ii = 1
-                a[ii] = a[ii] + 1
-                repeat(total) { yield(a).also { a.shift() } }
+    for (sugar in 0..100) {
+        for (sprinkles in 0..(100 - sugar)) {
+            for (candy in 0..(100 - sugar - sprinkles)) {
+                val choco = 100 - sugar - sprinkles - candy
+                if (choco < 0) continue
+
+                val capacity = maxOf(sugar * 3 + sprinkles * -3 - candy, 0)
+                val durability = maxOf(sprinkles * 3, 0)
+                val flavor = maxOf(candy * 4 + choco * -2, 0)
+                val texture = maxOf(sugar * -3 + choco * 2, 0)
+                val calories = maxOf(sugar * 2 + sprinkles * 9 + candy + choco * 8, 0)
+
+                if (withCalories && calories != 500) continue
+                res = maxOf(res, capacity * durability * flavor * texture)
             }
         }
     }
 
-    gen(3).onEach { println(it) }.toList()
-
     return res
-}
-
-private fun ArrayDeque<Int>.shift() {
-    addFirst(removeLast())
 }
