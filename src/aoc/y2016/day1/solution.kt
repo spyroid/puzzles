9cb96c9460e0bfd753b8153da1004d4b0a7ac296
Andrei Kovalevsky <spyroid@gmail.com>
2023-08-14 23:47:20 +0200

    .

---
+++ src/aoc/y2016/day1/solution.kt
@@ -0,0 +1,26 @@
+package aoc.y2016.day1
+
+import gears.Direction
+import gears.Point
+import gears.puzzle
+
+private fun main() {
+    puzzle { taxicab(linesFrom("input.txt")) }
+    puzzle { taxicab(linesFrom("input.txt"), true) }
+}
+
+private fun taxicab(lines: List<String>, twice: Boolean = false): Int {
+    var p = Point.zero
+    var dir = Direction.UP
+    val set = mutableSetOf(p)
+
+    lines.first().split(", ").onEach {
+        dir = if (it.first() == 'L') dir.turnCw() else dir.turnCcw()
+        repeat(it.drop(1).toInt()) {
+            p = p.plus(dir.asPoint())
+            if (twice && set.contains(p)) return p.manhattan()
+            set.add(p)
+        }
+    }
+    return p.manhattan()
+}
