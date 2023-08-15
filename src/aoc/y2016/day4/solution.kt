c2752c13f7495b68aa4c19abc34a30b28e573575
Andrei Kovalevsky <spyroid@gmail.com>
2023-08-16 01:06:04 +0200

    .

--- src/aoc/y2016/day4/solution.kt
+++ src/aoc/y2016/day4/solution.kt
@@ -8,21 +8,21 @@ private fun main() {
 }
 
 private fun obscurity2(lines: List<String>): Int {
-    return lines.map {
-        val s = it.substringBeforeLast("-")
-        val id = it.substringAfterLast("-").substringBefore("[").toInt()
-        id to s.map { c -> if (c == '-') ' ' else ((c.code - 'a'.code + id) % 26 + 'a'.code).toChar() }.joinToString("")
+    return lines.map { line ->
+        val (s, id) = parse(line)
+        id to s.map { if (it == '-') ' ' else ((it.code - 'a'.code + id) % 26 + 'a'.code).toChar() }.joinToString("")
     }.first { it.second == "northpole object storage" }.first
 }
 
 private fun obscurity(lines: List<String>): Int {
     return lines.sumOf {
-        val s = it.substringBeforeLast("-")
-        val id = it.substringAfterLast("-").substringBefore("[").toInt()
+        val (s, id) = parse(it)
         val checksum = it.substringAfterLast("[").dropLast(1)
         if (hash(s).startsWith(checksum)) id else 0
     }
 }
+
+private fun parse(s: String) = Pair(s.substringBeforeLast("-"), s.substringAfterLast("-").substringBefore("[").toInt())
 
 private fun hash(s: String): String {
     return s
