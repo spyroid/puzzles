5538c4ad69dad5633e1c1f5d40b700f0421afdd5
Andrei Kovalevsky <spyroid@gmail.com>
2023-08-24 01:14:35 +0200

    12

--- src/aoc/y2016/day12/solution.kt
+++ src/aoc/y2016/day12/solution.kt
@@ -11,12 +11,12 @@ private fun leo(lines: List<String>, c: Int): Int? {
     val regs = mutableMapOf("a" to 0, "b" to 0, "c" to c, "d" to 0)
     var ip = 0
     while (ip in lines.indices) {
-        val parts = lines[ip].split(" ")
-        when (parts[0]) {
-            "cpy" -> regs[parts[2]] = parts[1].toIntOrNull() ?: regs[parts[1]] ?: 0
-            "inc" -> regs[parts[1]] = (regs[parts[1]] ?: 0) + 1
-            "dec" -> regs[parts[1]] = (regs[parts[1]] ?: 0) - 1
-            "jnz" -> if (regs[parts[1]] != 0) ip += parts[2].toInt() - 1
+        val op = lines[ip].split(" ")
+        when (op[0]) {
+            "cpy" -> regs[op[2]] = op[1].toIntOrNull() ?: regs[op[1]] ?: 0
+            "inc" -> regs[op[1]] = (regs[op[1]] ?: 0) + 1
+            "dec" -> regs[op[1]] = (regs[op[1]] ?: 0) - 1
+            "jnz" -> if (regs[op[1]] != 0) ip += op[2].toInt() - 1
         }
         ip += 1
     }
