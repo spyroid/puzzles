205ded13b2b64096295b77c57efa97bc17870118
Andrei Kovalevsky <spyroid@gmail.com>
2023-08-25 01:22:09 +0200

    .

--- src/aoc/y2021/day13/Day13.kt
+++ src/aoc/y2021/day13/Day13.kt
@@ -1,76 +1,61 @@
 package aoc.y2021.day13
 
+import gears.Point
+import gears.puzzle
+
 fun main() {
-
-    data class Point(var x: Int, var y: Int)
-
-    class Paper(input: List<String>) {
-
-        var points = mutableSetOf<Point>()
-        val folds = mutableListOf<Point>()
-
-        init {
-            input.filter { it.isNotBlank() }.forEach { line ->
-                if (line.contains("fold")) {
-                    val s = line.split(" ").last().split("=")
-                    if (s[0] == "x") folds.add(Point(s[1].toInt(), 0)) else folds.add(Point(0, s[1].toInt()))
-                } else {
-                    line.split(",").map { it.toInt() }.let { points.add(Point(it[0], it[1])) }
-                }
-            }
-        }
-
-        fun fold() {
-            val fold = folds.removeFirst()
-            if (fold.x == 0) {
-                points = points.onEach { if (it.y > fold.y) it.y = fold.y * 2 - it.y }.toMutableSet()
-            } else {
-                points = points.onEach { if (it.x > fold.x) it.x = fold.x * 2 - it.x }.toMutableSet()
-            }
-        }
-
-        fun foldAll() {
-            while (folds.isNotEmpty()) fold()
-        }
-
-        override fun toString() = buildString {
-            for (y in 0..points.maxOf { it.y }) {
-                for (x in 0..points.maxOf { it.x }) {
-                    if (Point(x, y) in points) append("#") else append(" ")
-                }
-                appendLine()
-            }
-        }
-    }
-
-    fun part1(input: List<String>): Int {
-        val paper = Paper(input)
-        paper.fold()
-        return paper.points.size
-    }
-
-
-    fun part2(input: List<String>): String {
-        val paper = Paper(input)
-        paper.foldAll()
-        return paper.toString()
-    }
-
-//    val testData = readInput("day13/test")
-//    val inputData = readInput("day13/input")
-//
-//    var res1 = part1(testData)
-//    check(res1 == 17) { "Expected 17 but got $res1" }
-//
-//    var time = measureTimeMillis { res1 = part1(inputData) }
-//    println("⭐️ Part1: $res1 in $time ms")
-//
-//    var res2: String
-//    time = measureTimeMillis { res2 = part2(inputData) }
-//    println("⭐️ Part2: in $time ms\n\n\n\n\n")
-//    res2.split("\n").forEach {
-////        val c = term.colors.hsl(Random.nextInt(0, 25) * 10, 1_00, 60)
-//        println("\t\t" + it)
-//    }.let { println("\n\n") }
+    puzzle { part1(linesFrom("input.txt")) }
+    puzzle { part2(linesFrom("input.txt")) }
 }
 
+private class Paper(input: List<String>) {
+    var points = mutableSetOf<Point>()
+    val folds = mutableListOf<Point>()
+
+    init {
+        input.filter { it.isNotBlank() }.forEach { line ->
+            if (line.contains("fold")) {
+                val s = line.split(" ").last().split("=")
+                if (s[0] == "x") folds.add(Point(s[1].toInt(), 0)) else folds.add(Point(0, s[1].toInt()))
+            } else {
+                line.split(",").map { it.toInt() }.let { points.add(Point(it[0], it[1])) }
+            }
+        }
+    }
+
+    fun fold() {
+        val fold = folds.removeFirst()
+        if (fold.x == 0) {
+            points = points.onEach { if (it.y > fold.y) it.y = fold.y * 2 - it.y }.toMutableSet()
+        } else {
+            points = points.onEach { if (it.x > fold.x) it.x = fold.x * 2 - it.x }.toMutableSet()
+        }
+    }
+
+    fun foldAll() {
+        while (folds.isNotEmpty()) fold()
+    }
+
+    override fun toString() = buildString {
+        for (y in 0..points.maxOf { it.y }) {
+            for (x in 0..points.maxOf { it.x }) {
+                if (Point(x, y) in points) append("#") else append(" ")
+            }
+            appendLine()
+        }
+    }
+}
+
+private fun part1(input: List<String>): Int {
+    val paper = Paper(input)
+    paper.fold()
+    return paper.points.size
+}
+
+
+private fun part2(input: List<String>): String {
+    val paper = Paper(input)
+    paper.foldAll()
+    println(paper.toString())
+    return ""
+}
--- src/aoc/y2021/day14/Day14.kt
+++ src/aoc/y2021/day14/Day14.kt
@@ -1,75 +1,65 @@
 package aoc.y2021.day14
 
+import gears.puzzle
+
 fun main() {
+    puzzle { part1(linesFrom("input.txt")) }
+    puzzle { part2(linesFrom("input.txt")) }
+}
 
-    class Polymer(input: List<String>) {
-        var template = input.first()
+private class Polymer(input: List<String>) {
+    var template = input.first()
 
-        val pairCounts = input.first().asSequence()
-            .windowed(2)
-            .map { it.joinToString("") }
-            .groupingBy { it }
-            .eachCount()
-            .mapValues { (_, v) -> v.toLong() }
+    val pairCounts = input.first().asSequence()
+        .windowed(2)
+        .map { it.joinToString("") }
+        .groupingBy { it }
+        .eachCount()
+        .mapValues { (_, v) -> v.toLong() }
 
-        val spawnRules = input.drop(2).associate {
-            it.split(" -> ").let { (pair, insertion) ->
-                pair to Pair(pair.first() + insertion, insertion + pair.last())
+    val spawnRules = input.drop(2).associate {
+        it.split(" -> ").let { (pair, insertion) ->
+            pair to Pair(pair.first() + insertion, insertion + pair.last())
+        }
+    }
+
+    fun calculate(iterations: Int): Long {
+        return generateSequence(pairCounts) { getNewCounts(it) }
+            .drop(iterations)
+            .first()
+            .let { countLetters(it) }
+            .values
+            .sortedBy { it }.let { it.last() - it.first() }
+    }
+
+    private fun countLetters(pairCounts: Map<String, Long>): Map<Char, Long> {
+        val letterCounts = mutableMapOf<Char, Long>().apply {
+            pairCounts.entries.forEach { (key, value) ->
+                this[key.first()] = getOrDefault(key.first(), 0) + value
+                this[key.last()] = getOrDefault(key.last(), 0) + value
             }
         }
-
-        fun calculate(iterations: Int): Long {
-            return generateSequence(pairCounts) { getNewCounts(it) }
-                .drop(iterations)
-                .first()
-                .let { countLetters(it) }
-                .values
-                .sortedBy { it }.let { it.last() - it.first() }
-        }
-
-        private fun countLetters(pairCounts: Map<String, Long>): Map<Char, Long> {
-            val letterCounts = mutableMapOf<Char, Long>().apply {
-                pairCounts.entries.forEach { (key, value) ->
-                    this[key.first()] = getOrDefault(key.first(), 0) + value
-                    this[key.last()] = getOrDefault(key.last(), 0) + value
-                }
-            }
-            return letterCounts.entries.associate { (key, value) ->
-                when (key) {
-                    template.first(), template.last() -> key to (value + 1) / 2
-                    else -> key to value / 2
-                }
-            }
-        }
-
-        private fun getNewCounts(pairCounts: Map<String, Long>): Map<String, Long> {
-            return mutableMapOf<String, Long>().apply {
-                pairCounts.forEach { pair ->
-                    val pp = spawnRules[pair.key]
-                    if (pp != null) {
-                        this[pp.first] = getOrDefault(pp.first, 0L) + pair.value
-                        this[pp.second] = getOrDefault(pp.second, 0L) + pair.value
-                    }
-                }
+        return letterCounts.entries.associate { (key, value) ->
+            when (key) {
+                template.first(), template.last() -> key to (value + 1) / 2
+                else -> key to value / 2
             }
         }
     }
 
-    fun part1(input: List<String>) = Polymer(input).calculate(10)
-
-    fun part2(input: List<String>) = Polymer(input).calculate(40)
-
-
-//    val testData = readInput("day14/test")
-//    val inputData = readInput("day14/input")
-//
-//    var res1 = part1(testData)
-//    check(res1 == 1588L) { "Expected 1588 but got $res1" }
-//
-//    var time = measureTimeMillis { res1 = part1(inputData) }
-//    println("⭐️ Part1: $res1 in $time ms")
-//
-//    time = measureTimeMillis { res1 = part2(inputData) }
-//    println("⭐️ Part2: $res1 in $time ms")
+    private fun getNewCounts(pairCounts: Map<String, Long>): Map<String, Long> {
+        return mutableMapOf<String, Long>().apply {
+            pairCounts.forEach { pair ->
+                val pp = spawnRules[pair.key]
+                if (pp != null) {
+                    this[pp.first] = getOrDefault(pp.first, 0L) + pair.value
+                    this[pp.second] = getOrDefault(pp.second, 0L) + pair.value
+                }
+            }
+        }
+    }
 }
 
+private fun part1(input: List<String>) = Polymer(input).calculate(10)
+
+private fun part2(input: List<String>) = Polymer(input).calculate(40)
