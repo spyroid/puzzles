504ddbc441754ad913f1121f86cc297dafa26125
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-20 21:05:32 +0200

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "1.9.20-Beta"
+    kotlin("jvm") version "1.9.20-Beta2"
     id("com.github.ben-manes.versions") version "0.48.0"
 }
 
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 //    implementation("com.github.ajalt.mordant:mordant:2.0.0-beta13")
 //    implementation("org.jetbrains.kotlinx:multik-api:0.2.1")
 //    implementation("org.jetbrains.kotlinx:multik-default:0.2.1")
