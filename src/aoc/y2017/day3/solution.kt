3d6c905d4d2f3e63730d93ced4eedd770a03041c
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-23 01:26:41 +0200

    .

--- src/aoc/y2017/day3/solution.kt
+++ src/aoc/y2017/day3/solution.kt
@@ -2,14 +2,19 @@ package aoc.y2017.day3
 
 import gears.puzzle
 import kotlin.math.absoluteValue
+import kotlin.math.sqrt
 
 private fun main() {
     puzzle { spiral(368078) }
+    puzzle { spiral11(22) }
+}
+
+fun spiral11(input: Int) {
+    println(sqrt(input.toDouble()))
 }
 
 private fun spiral(input: Int): Int {
-    var side = 1
-    while (true) if (input < side.times(side)) break else side += 2
+    val side = generateSequence(1) { it + 2 }.first { input < it * it }
     val level = (side - 1) / 2
     var a = side * side
     while (a > input) a -= side - 1
