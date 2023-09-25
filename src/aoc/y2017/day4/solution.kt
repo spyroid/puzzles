15086f616b56944a69314eee133627bb1fd350ae
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-25 20:55:58 +0200

    .

---
+++ src/aoc/y2017/day4/solution.kt
@@ -0,0 +1,17 @@
+package aoc.y2017.day4
+
+import gears.puzzle
+
+private fun main() {
+    puzzle { phrase(linesFrom("input.txt")) }
+    puzzle { phrase(linesFrom("input.txt"), true) }
+}
+
+private fun phrase(input: List<String>, part2: Boolean = false) = input.map { line ->
+    line.split(" ")
+        .map { if (part2) it.toList().sorted().joinToString("") else it }
+        .groupingBy { it }
+        .eachCount()
+        .count { it.value > 1 } == 0
+}.count { it }
+
