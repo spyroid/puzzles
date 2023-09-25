bda177c8db9f6247f36b19a7f75cb6dcc0d54891
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-25 21:21:42 +0200

    .

---
+++ src/aoc/y2017/day5/solution.kt
@@ -0,0 +1,22 @@
+package aoc.y2017.day5
+
+import gears.puzzle
+import gears.toInts
+
+private fun main() {
+    puzzle { maze(linesFrom("input.txt").toInts()) }
+    puzzle { maze(linesFrom("input.txt").toInts(), true) }
+}
+
+private fun maze(input: List<Int>, part2: Boolean = false): Int {
+    val program = input.toMutableList()
+    var steps = 0
+    var ip = 0
+    while (ip in program.indices) {
+        ip += program[ip].also {
+            if (part2 && program[ip] >= 3) program[ip] = program[ip] - 1 else program[ip] = program[ip] + 1
+        }
+        steps++
+    }
+    return steps
+}
