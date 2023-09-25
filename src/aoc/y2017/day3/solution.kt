10dfb8144d97343bb55319284ba831a9a3c4521b
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-25 20:35:03 +0200

    .

--- src/aoc/y2017/day3/solution.kt
+++ src/aoc/y2017/day3/solution.kt
@@ -1,16 +1,29 @@
 package aoc.y2017.day3
 
+import gears.Direction.*
+import gears.Point
 import gears.puzzle
 import kotlin.math.absoluteValue
-import kotlin.math.sqrt
 
 private fun main() {
     puzzle { spiral(368078) }
-    puzzle { spiral11(22) }
+    puzzle { spiral2(368078) }
 }
 
-fun spiral11(input: Int) {
-    println(sqrt(input.toDouble()))
+fun spiral2(input: Int): Int {
+    val map = mutableMapOf(Point(0, 0) to 1)
+    var sideLength = 3
+    var p = Point(1, 0)
+    while (true) {
+        for ((length, dir) in listOf(sideLength - 2 to UP, sideLength - 1 to LEFT, sideLength - 1 to DOWN, sideLength to RIGHT)) {
+            repeat(length) {
+                val v = p.around8().sumOf { map[it] ?: 0 }
+                if (v > input) return v else map[p] = v
+                p += dir.asPoint()
+            }
+        }
+        sideLength += 2
+    }
 }
 
 private fun spiral(input: Int): Int {
