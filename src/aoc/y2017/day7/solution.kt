66fada23ff631ea796ff67a9c395b2717fbf6275
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-26 11:06:30 +0200

    .

---
+++ src/aoc/y2017/day7/solution.kt
@@ -0,0 +1,23 @@
+package aoc.y2017.day7
+
+import gears.puzzle
+
+private fun main() {
+    puzzle { circus(linesFrom("input.txt")) }
+}
+
+private fun circus(input: List<String>, part2: Boolean = false): String {
+
+    val all = input.map { line ->
+        val (name, a) = line.split(" (")
+        val (age, b) = a.split(")")
+        val others = if (b.contains("->")) b.split(" -> ").let { it.last().split(", ") } else listOf()
+        Triple(name, age, others.toSet())
+    }
+
+    val names = all.map { it.first }.toMutableSet().also {
+        all.forEach { e -> it.removeAll(e.third) }
+    }
+    return names.first()
+}
+
