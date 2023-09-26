8635a7afdc512aa19a1e3deaf2d5f711cc82359c
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-26 20:19:36 +0200

    .

--- src/aoc/y2017/day7/solution.kt
+++ src/aoc/y2017/day7/solution.kt
@@ -4,20 +4,41 @@ import gears.puzzle
 
 private fun main() {
     puzzle { circus(linesFrom("input.txt")) }
+    puzzle { circus(linesFrom("input.txt"), true) }
 }
 
-private fun circus(input: List<String>, part2: Boolean = false): String {
-
+private fun circus(input: List<String>, part2: Boolean = false): Any {
     val all = input.map { line ->
         val (name, a) = line.split(" (")
         val (age, b) = a.split(")")
         val others = if (b.contains("->")) b.split(" -> ").last().split(", ") else listOf()
-        Triple(name, age, others.toSet())
+        Node(name, age.toInt(), others)
     }
+    val lookup = all.associateBy { it.name }
+    val root = all.map { it.name }.toMutableSet().also { all.forEach { e -> it.removeAll(e.others.toSet()) } }.first()
 
-    val names = all.map { it.first }.toMutableSet().also {
-        all.forEach { e -> it.removeAll(e.third) }
+    if (!part2) return root
+
+    updatedWeights(lookup[root]!!, lookup)
+
+    for (p in lookup.values) {
+        val weights = p.others.map { lookup[it] }.groupBy { it!!.weight }
+        if (weights.size > 1) {
+            val correctWeight = weights.filterValues { it.size > 1 }.keys.toList()[0]
+            val wrongWeight = weights.filterValues { it.size == 1 }.keys.toList()[0]
+            val diff = correctWeight - wrongWeight
+            return (weights[wrongWeight]!![0]?.oWeight ?: 0) + if (correctWeight > wrongWeight) -diff else diff
+        }
     }
-    return names.first()
+    return 0
 }
+
+private fun updatedWeights(n: Node, lookup: Map<String, Node>) {
+    for (support in n.others) {
+        updatedWeights(lookup[support]!!, lookup)
+        lookup[n.name]!!.weight += lookup[support]!!.weight
+    }
+}
+
+private data class Node(val name: String, val oWeight: Int, val others: List<String> = listOf(), var weight: Int = oWeight)
 
