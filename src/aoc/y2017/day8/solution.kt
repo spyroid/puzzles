5114a3b26b05f8dcbec17eb1f245de118f62743b
Andrei Kovalevsky <spyroid@gmail.com>
2023-09-29 11:28:15 +0200

    .

--- src/aoc/y2017/day8/solution.kt
+++ src/aoc/y2017/day8/solution.kt
@@ -6,14 +6,13 @@ private fun main() {
     puzzle { registers(inputLines()) }
 }
 
-private val re = "(\\S+) (\\S+) (\\S+) if (\\S+) (\\S+) (\\S+)".toRegex()
-private fun registers(input: List<String>, part2: Boolean = false): Any {
+private val re = "\\S+".toRegex()
+private fun registers(input: List<String>): Any {
     val map = mutableMapOf<String, Int>()
     var max = 0
     input.map {
-        re.findAll(it).map { a -> a.groupValues.drop(1) }
-            .first()
-            .let { b -> Command(b[0], if (b[1] == "inc") b[2].toInt() else -b[2].toInt(), b[3], b[4], b[5].toInt()) }
+        re.findAll(it).map { a -> a.value }.toList()
+            .let { b -> Command(b[0], if (b[1] == "inc") b[2].toInt() else -b[2].toInt(), b[4], b[5], b[6].toInt()) }
     }
         .forEach { c ->
             val r2 = map.getOrPut(c.reg2) { 0 }
@@ -28,7 +27,6 @@ private fun registers(input: List<String>, part2: Boolean = false): Any {
             if (flag) map.compute(c.reg1) { _, v -> (v ?: 0) + c.v1 }.also { max = maxOf(it ?: 0, max) }
         }
     return map.values.maxByOrNull { it } to max
-
 }
 
 private data class Command(val reg1: String, val v1: Int, val reg2: String, val op2: String, val v2: Int)
---
+++ src/aoc/y2017/day9/solution.kt
@@ -0,0 +1,16 @@
+package aoc.y2017.day9
+
+import gears.puzzle
+
+private fun main() {
+    puzzle { stream(input()) }
+}
+
+private fun stream(input: String): Any {
+    var garbage = 0
+    var s = input.replace("!\\S".toRegex(), "")
+    while (s.contains('<')) s = s.removeRange(s.indexOf('<'), s.indexOf('>') + 1).also { garbage += s.length - it.length - 2 }
+    var stack = 0
+    var sum = s.filter { it != ',' }.sumOf { c -> if (c == '{') stack++.let { 0 } else stack-- }
+    return sum to garbage
+}
