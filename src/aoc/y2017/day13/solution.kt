aa5e34e921048e715ba221a942a152ea532562fc
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-02 01:15:12 +0200

    .

--- src/aoc/y2017/day13/solution.kt
+++ src/aoc/y2017/day13/solution.kt
@@ -8,13 +8,31 @@ private fun main() {
 
 private fun scanner(input: List<String>): Any {
     val all = input.map { it.split(": ").map { a -> a.toInt() } }.associate { it.first() to Scanner(it.last()) }
-    var severity = 0
-    for (idx in 0..all.keys.max()) {
-        val s = all[idx]
-        if (s?.level == 0) severity += idx * s.size
-        all.values.forEach { it.step() }
+    fun severity(delay: Int): Int {
+        var severity = 0
+        all.values.forEach { it.reset() }
+        repeat(delay) { all.values.forEach { it.step() } }
+        for (idx in 0..all.keys.max()) {
+            val s = all[idx]
+            if (s?.level == 0) severity += idx * s.size
+            all.values.forEach { it.step() }
+        }
+        return severity
     }
-    return severity
+//    println(severity(0))
+//    println(all)
+    var i = 0
+    val part1 = 99//severity(0)
+    val list = mutableListOf<Int>()
+    while (true) {
+        val s = severity(i)
+        if (s == 0) {
+            list.add(i)
+        }
+        if (list.size > 1) break
+        i++
+    }
+    return part1 to i
 }
 
 private data class Scanner(val size: Int, var level: Int = 0) {
@@ -22,5 +40,9 @@ private data class Scanner(val size: Int, var level: Int = 0) {
     fun step() {
         if (level + dir !in 0..<size) dir = -dir
         level += dir
+    }
+    fun reset() {
+        dir = 1
+        level = 0
     }
 }
