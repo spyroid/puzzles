2a80afa03b8778966c5694a0917b1debd5891897
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-01 03:02:55 +0200

    .

--- src/aoc/y2017/day11/solution.kt
+++ src/aoc/y2017/day11/solution.kt
@@ -9,11 +9,8 @@ private fun main() {
 
 private fun hexed(input: List<String>): Any {
     var (s, q, r) = Triple(0, 0, 0)
-    var max = 0
-
     fun dist() = (abs(s) + abs(q) + abs(r)) / 2
-
-    input.forEach {
+    val max = input.maxOf {
         when (it) {
             "n" -> s = s.inc().also { r = r.dec() }
             "s" -> s = s.dec().also { r = r.inc() }
@@ -22,7 +19,7 @@ private fun hexed(input: List<String>): Any {
             "nw" -> s = s.inc().also { q = q.dec() }
             "se" -> s = s.dec().also { q = q.inc() }
         }
-        max = maxOf(max, dist())
+        dist()
     }
     return dist() to max
 }
