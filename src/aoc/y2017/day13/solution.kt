2adca26f322768e66b82993bae5e8ed77cc230c8
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-03 23:34:46 +0200

    .

--- src/aoc/y2017/day13/solution.kt
+++ src/aoc/y2017/day13/solution.kt
@@ -7,42 +7,13 @@ private fun main() {
 }
 
 private fun scanner(input: List<String>): Any {
-    val all = input.map { it.split(": ").map { a -> a.toInt() } }.associate { it.first() to Scanner(it.last()) }
-    fun severity(delay: Int): Int {
-        var severity = 0
-        all.values.forEach { it.reset() }
-        repeat(delay) { all.values.forEach { it.step() } }
-        for (idx in 0..all.keys.max()) {
-            val s = all[idx]
-            if (s?.level == 0) severity += idx * s.size
-            all.values.forEach { it.step() }
-        }
-        return severity
-    }
-//    println(severity(0))
-//    println(all)
-    var i = 0
-    val part1 = 99//severity(0)
-    val list = mutableListOf<Int>()
-    while (true) {
-        val s = severity(i)
-        if (s == 0) {
-            list.add(i)
-        }
-        if (list.size > 1) break
-        i++
-    }
-    return part1 to i
+    val all = input.map { it.split(": ").map { a -> a.toInt() } }.map { Layer(it.first(), it.last()) }
+    val part1 = all.filter { it.caughtAt(0) }.sumOf { it.severity }
+    val part2 = generateSequence(0, Int::inc).filter { time -> all.none { it.caughtAt(time) } }.first()
+    return part1 to part2
 }
 
-private data class Scanner(val size: Int, var level: Int = 0) {
-    private var dir = 1
-    fun step() {
-        if (level + dir !in 0..<size) dir = -dir
-        level += dir
-    }
-    fun reset() {
-        dir = 1
-        level = 0
-    }
+private data class Layer(val depth: Int, val range: Int = 0) {
+    fun caughtAt(time: Int) = (time + depth) % ((range - 1) * 2) == 0
+    val severity = depth * range
 }
