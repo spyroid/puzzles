4b0053f0919ba9b6cd08c4510f5021da7aa7de54
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-08 01:19:05 +0200

    .

--- src/aoc/y2017/day15/solution.kt
+++ src/aoc/y2017/day15/solution.kt
@@ -7,14 +7,13 @@ private fun main() {
     puzzle { duel2(618, 814) }
 }
 
-private fun gen(start: Long, factor: Long) = generateSequence(start) { it * factor % 2147483647 }
+private fun gen(start: Long, factor: Long) = generateSequence(start) { it * factor % 2147483647 }.map { it.toShort() }
 
-private fun count(repeat: Int, seq: Sequence<Pair<Long, Long>>) = seq
-    .map { (a, b) -> a.and(0xffff) == b.and(0xffff) }
-    .take(repeat)
-    .count { it }
+private fun count(repeat: Int, seq: Sequence<Pair<Short, Short>>) = seq.take(repeat).count { it.first == it.second }
 
 private fun duel(a: Long, b: Long) = count(40_000_000, gen(a, 16807).zip(gen(b, 48271)))
 
-private fun duel2(a: Long, b: Long) = gen(a, 16807).filter { it % 4 == 0L }.zip(gen(b, 48271).filter { it % 8 == 0L })
+private fun duel2(a: Long, b: Long) = gen(a, 16807)
+    .filter { it % 4 == 0 }
+    .zip(gen(b, 48271).filter { it % 8 == 0 })
     .let { count(5_000_000, it) }
