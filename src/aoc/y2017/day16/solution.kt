d883b666101698de7f03facd603af86790fdc120
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-08 16:15:19 +0200

    .

--- src/aoc/y2017/day16/solution.kt
+++ src/aoc/y2017/day16/solution.kt
@@ -3,22 +3,28 @@ package aoc.y2017.day16
 import gears.puzzle
 
 private fun main() {
-    puzzle { permutation(input().trim().split(",")) }
+    puzzle { permutation(input().trim().split(","), 1) }
+    puzzle { permutation(input().trim().split(","), 1_000_000_000) }
 }
 
-private fun permutation(input: List<String>): Any {
+private fun permutation(input: List<String>, times: Int): Any {
     var programs = ('a'..'p').toMutableList()
-    for (move in input) {
-        val op = move.first()
-        val (b, c) = move.drop(1).split("/").plus("999").let { (bb, cc) ->
-            if (op == 'p') programs.indexOf(bb.first()) to programs.indexOf(cc.first()) else bb.toInt() to cc.toInt()
+    val seen = mutableListOf<String>()
+
+    repeat(times) {
+        if (seen.contains(programs.joinToString(""))) {
+            return seen[times % seen.size]
+        } else {
+            seen.add(programs.joinToString(""))
         }
-        when (op) {
-            's' -> programs = programs.drop(programs.size - b).plus(programs.dropLast(b)).toMutableList()
-            else -> {
-                val q = programs[b]
-                programs[b] = programs[c]
-                programs[c] = q
+        for (move in input) {
+            val op = move.first()
+            val (b, c) = move.drop(1).split("/").plus("999").let { (bb, cc) ->
+                if (op == 'p') programs.indexOf(bb.first()) to programs.indexOf(cc.first()) else bb.toInt() to cc.toInt()
+            }
+            when (op) {
+                's' -> programs = programs.drop(programs.size - b).plus(programs.dropLast(b)).toMutableList()
+                else -> programs[b] = programs[c].also { programs[c] = programs[b] }
             }
         }
     }
