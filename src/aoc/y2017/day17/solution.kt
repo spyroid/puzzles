dc7eefc17aa26763103a00842ec6473a889da33c
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-09 01:26:46 +0200

    .

--- src/aoc/y2017/day17/solution.kt
+++ src/aoc/y2017/day17/solution.kt
@@ -9,10 +9,14 @@ private fun main() {
 private fun spinlock(step: Int): Any {
     val spin = mutableListOf(0)
     var pos = 0
+    var part1 = 0
+    var part2 = 0
 
-    for (i in 1..2017) {
-        pos = (pos + step) % spin.size + 1
-        spin.add(pos, i)
+    for (i in 1..50_000_000) {
+        pos = (pos + step) % i + 1
+        if (pos == 1) part2 = i
+        if (pos <= 2017) spin.add(pos, i)
+        if (i == 2017) part1 = spin[pos + 1]
     }
-    return spin[pos + 1]
+    return part1 to part2
 }
