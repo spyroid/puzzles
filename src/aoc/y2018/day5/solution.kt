4c64c87a317a07823ecd55997c588296bd4d7c1e
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-17 23:41:13 +0200

    .

--- src/aoc/y2018/day5/solution.kt
+++ src/aoc/y2018/day5/solution.kt
@@ -5,18 +5,20 @@ import kotlin.math.abs
 
 private fun main() {
     puzzle { reduction(inputLines().first()) }
+    puzzle { reduction2(inputLines().first()) }
 }
 
-private fun reduction(input: String): Any {
-    val all = input.toMutableList()
+private fun reduction2(input: String) = input.toSet()
+    .filter { it in 'A'..'Z' }
+    .minOf { c -> reduction(input.filter { it != c && it != c + 32 }) }
 
-    println('a' - 'A')
+private fun reduction(input: String): Int {
+    val all = input.toMutableList()
     while (true) {
         var f = false
         for (i in 0..<all.lastIndex - 1) {
             if (abs(all[i] - all[i + 1]) == 32) {
-                all.removeAt(i)
-                all.removeAt(i)
+                repeat(2) { all.removeAt(i) }
                 f = true
                 break
             }
