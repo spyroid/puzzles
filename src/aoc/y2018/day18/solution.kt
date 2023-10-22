ab781abe00fa8e25d99637f4378c86a25c337538
Andrei Kovalevsky <spyroid@gmail.com>
2023-10-22 21:14:01 +0200

    .

--- src/aoc/y2018/day18/solution.kt
+++ src/aoc/y2018/day18/solution.kt
@@ -8,6 +8,10 @@ private fun main() {
     puzzle { pole2(inputLines()) }
 }
 
+private fun seq(grid: Grid<Char>): Sequence<Grid<Char>> = generateSequence(grid) {
+    evolve(it)
+}
+
 private fun evolve(grid: Grid<Char>) = grid.clone { x, y, e ->
     val around = grid.around8(x, y)
     when (e) {
@@ -17,23 +21,23 @@ private fun evolve(grid: Grid<Char>) = grid.clone { x, y, e ->
     }
 }
 
-private fun pole(input: List<String>): Any {
-    var grid = Grid.of(input) { it }
-    repeat(10) { grid = evolve(grid) }
-    return grid.all().count { it == '|' } * grid.all().count { it == '#' }
+private fun pole(input: List<String>) = seq(Grid.of(input) { it })
+    .drop(10)
+    .first()
+    .let { grid ->
+    grid.all().count { it == '|' } * grid.all().count { it == '#' }
 }
 
-private fun pole2(input: List<String>): Any {
-    var grid = Grid.of(input) { it }
+private fun pole2(input: List<String>): Any { // 174420
     val seen = mutableMapOf<Int, Int>()
     var gen = 0
-    while (true) {
+    var grid = seq(Grid.of(input) { it }).dropWhile { grid ->
         val hash = grid.deepHashCode()
-        if (hash in seen) break else seen[hash] = gen++
-        grid = evolve(grid)
-    }
+        if (hash in seen) return@dropWhile false else seen[hash] = gen++
+        true
+    }.first()
     val cycle = gen - seen[grid.deepHashCode()]!!
     val moreSteps = (1_000_000_000 - gen) % cycle
-    repeat(moreSteps) { grid = evolve(grid) }
+    grid = seq(grid).drop(moreSteps).first()
     return grid.all().count { it == '|' } * grid.all().count { it == '#' }
 }
