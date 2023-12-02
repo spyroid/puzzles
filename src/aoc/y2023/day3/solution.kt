d96fd392a69681e9ea9f4684f356898c219acc2b
Andrei Kovalevsky <spyroid@gmail.com>
2023-12-02 15:18:23 +0100

    .

---
+++ src/aoc/y2023/day3/solution.kt
@@ -0,0 +1,11 @@
+package aoc.y2023.day3
+
+import gears.puzzle
+
+private fun main() {
+    puzzle { conundrum(inputLines()) }
+}
+
+private fun conundrum(inputLines: List<String>): Any {
+    return 1
+}
