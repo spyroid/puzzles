339a6bc4fc362462d735e40de40169b76a93e560
Andrei Kovalevsky <spyroid@gmail.com>
2023-12-03 15:03:00 +0100

    .

--- src/aoc/y2023/day3/solution.kt
+++ src/aoc/y2023/day3/solution.kt
@@ -1,6 +1,7 @@
 package aoc.y2023.day3
 
 import gears.Grid
+import gears.Point
 import gears.puzzle
 
 private fun main() {
@@ -11,14 +12,23 @@ private fun gear(input: List<String>): Any {
     val grid = Grid.of(input) { it }
     var number = ""
     val around = mutableSetOf<Char>()
+    val gears = mutableSetOf<Point>()
+    val g2n = mutableMapOf<Point, MutableSet<Int>>()
     return grid.all().mapNotNull { (p, c) ->
         if (c.isDigit()) {
-            around.addAll(p.around8().mapNotNull { grid.at(it) }).also { number += c }
+            around.addAll(p.around8().mapNotNull {
+                val c = grid.at(it)
+                if (c == '*') gears.add(it)
+                c
+            }).also { number += c }
         } else if (number.isNotEmpty()) {
-            val res = if (around.any { it != '.' && !it.isDigit() }) number else null
+            val res = if (around.any { it != '.' && !it.isDigit() }) {
+                gears.onEach { g2n.getOrPut(it) { mutableSetOf() }.add(number.toInt()) }.clear()
+                number.toInt()
+            } else null
             around.clear().also { number = "" }
             return@mapNotNull res
         }
         null
-    }.sumOf { it.toInt() }
+    }.sumOf { it } to g2n.values.filter { it.size == 2 }.sumOf { it.first() * it.last() }
 }
