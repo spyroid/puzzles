7afd757c085e874768daaf4d75e212fd2bbebc9f
Andrei Kovalevsky <spyroid@gmail.com>
2023-12-04 19:07:51 +0100

    .

---
+++ src/aoc/y2023/day4/solution.kt
@@ -0,0 +1,20 @@
+package aoc.y2023.day4
+
+import gears.findIntNumbers
+import gears.puzzle
+import gears.splitBy
+
+private fun main() {
+    puzzle { scratch(inputLines()) }
+}
+
+private fun scratch(input: List<String>): Any {
+    val cards = mutableMapOf<Int, Int>()
+    return input.map { card ->
+        val (id, a, b) = card.findIntNumbers().splitBy(setOf(1, 11))
+        val matches = (a intersect b.toSet()).size
+        val m = cards.compute(id.first()) { _, v -> (v ?: 0) + 1 } ?: 0
+        (1..matches).forEach { i -> cards.compute(id.first() + i) { _, v -> (v ?: 0) + m } }
+        1 shl matches - 1
+    }.sumOf { it } to cards.values.sum()
+}
