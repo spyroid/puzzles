9f8467383dd7eb7a59c224374d7d2bbe1a1f8d9d
spyroid <soyroid@gmail.com>
2023-12-20 23:01:04 +0100

    .

--- src/aoc/y2023/day15/solution.kt
+++ src/aoc/y2023/day15/solution.kt
@@ -3,16 +3,16 @@ package aoc.y2023.day15
 import gears.puzzle
 
 private fun main() {
-    puzzle("1 & 2") { lensLibrary(input().split(",")) }
+    puzzle("1 & 2") { lensLibrary(input()) }
 }
 
-private fun lensLibrary(lenses: List<String>): Any {
+private fun lensLibrary(input: String): Any {
+    val lenses = input.split(",")
     val part1 = lenses.sumOf { it.hash() }
-
     val part2 = buildMap<String, Int>() {
         lenses.onEach { s -> if (s.contains('-')) remove(s.substringBefore('-')) else s.split('=').let { (a, b) -> this[a] = b.toInt() } }
     }.entries.groupBy { it.key.hash() }
-        .map { (it.key + 1) * it.value.foldIndexed(0) { i, acc, lense -> acc + (i + 1) * lense.value } }
+        .map { (it.key + 1) * it.value.foldIndexed(0) { i, acc, lens -> acc + (i + 1) * lens.value } }
         .sum()
 
     return part1 to part2
