ce02e783d1c0cccca98046f9a886ad394c6eb70d
spyroid <soyroid@gmail.com>
2023-12-20 22:51:28 +0100

    .

--- src/aoc/y2023/day15/solution.kt
+++ src/aoc/y2023/day15/solution.kt
@@ -3,22 +3,19 @@ package aoc.y2023.day15
 import gears.puzzle
 
 private fun main() {
-    puzzle("1 & 2") { lensLibrary(input()) }
+    puzzle("1 & 2") { lensLibrary(input().split(",")) }
 }
 
-private fun lensLibrary(input: String): Any {
-    val data = input.split(",")
-    val part1 = data.sumOf { Key(it).hashCode() }
+private fun lensLibrary(lenses: List<String>): Any {
+    val part1 = lenses.sumOf { it.hash() }
 
-    val part2 = buildMap<Key, Int>() {
-        data.onEach { s -> if (s.contains('-')) remove(Key(s.substringBefore('-'))) else s.split('=').let { (a, b) -> this[Key(a)] = b.toInt() } }
-    }.entries.groupBy { it.key.hashCode() }
-        .map { (it.key + 1) * it.value.foldIndexed(0) { i, acc, lens -> acc + (i + 1) * lens.value } }
+    val part2 = buildMap<String, Int>() {
+        lenses.onEach { s -> if (s.contains('-')) remove(s.substringBefore('-')) else s.split('=').let { (a, b) -> this[a] = b.toInt() } }
+    }.entries.groupBy { it.key.hash() }
+        .map { (it.key + 1) * it.value.foldIndexed(0) { i, acc, lense -> acc + (i + 1) * lense.value } }
         .sum()
 
     return part1 to part2
 }
 
-private data class Key(val id: String) {
-    override fun hashCode() = id.fold(0) { acc, c -> (acc + c.code) * 17 % 256 }
-}
+private fun String.hash() = this.fold(0) { acc, c -> (acc + c.code) * 17 % 256 }
