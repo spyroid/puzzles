ef63a8fda559cfff187fb609ee3c8315ffbb395a
spyroid <soyroid@gmail.com>
2024-01-16 02:39:34 +0100

    .

---
+++ src/aoc/y2019/day3/solution.kt
@@ -0,0 +1,27 @@
+package aoc.y2019.day3
+
+import gears.Direction
+import gears.Point
+import gears.puzzle
+
+private fun main() {
+    puzzle("1 & 2") { crossedWires(inputLines()) }
+}
+
+private fun crossedWires(input: List<String>): Any {
+    fun walk(path: String): Set<Point> = buildSet {
+        var p = Point.zero
+        var steps = 0
+        path.split(",").forEach { s ->
+            val (dir, length) = Direction.of(s.first()) to s.drop(1).toInt()
+            p = (0..<length).fold(p) { pp, _ -> pp.plus(dir).also { it.value = ++steps }.also { add(it) } }
+        }
+    }
+
+    val a = walk(input.first())
+    val b = walk(input.last())
+    val c = a.intersect(b)
+
+    val part2 = c.minOf { p -> a.first { it == p }.value + b.first { it == p }.value }
+    return c.minOf { it.manhattan() } to part2
+}
