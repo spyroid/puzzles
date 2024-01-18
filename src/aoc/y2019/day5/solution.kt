c435c71104de5ad202c9664f217fb6fae083154b
spyroid <soyroid@gmail.com>
2024-01-18 22:14:31 +0100

    .

--- src/aoc/y2019/day5/solution.kt
+++ src/aoc/y2019/day5/solution.kt
@@ -4,15 +4,15 @@ import gears.findInts
 import gears.puzzle
 
 private fun main() {
-    puzzle("1 & 2") { sunnyChanceAsteroids(input().findInts()) }
+    puzzle("1") { sunnyChanceAsteroids(input().findInts(), 1) }
+    puzzle("2") { sunnyChanceAsteroids(input().findInts(), 5) }
 }
 
-private fun sunnyChanceAsteroids(data: List<Int>): Any {
+private fun sunnyChanceAsteroids(data: List<Int>, input: Int): Any {
     var (program, ip) = data.toTypedArray() to 0
-    val input = 1
     var output = 0
 
-    fun read(addr: Int) = program.getOrNull(addr) ?: -999
+    fun read(addr: Int) = program.getOrElse(addr) { -99999 }
     fun readAt(offset: Int = 0, flags: List<Boolean> = emptyList()): Int {
         val p = read(ip + offset)
         return if (flags.isNotEmpty() && !flags[offset - 1]) read(p) else p
@@ -28,21 +28,38 @@ private fun sunnyChanceAsteroids(data: List<Int>): Any {
         when (inst.op) {
             1 -> {
                 writeAt(addr, a + b)
-                ip += 2
+                ip += 4
             }
 
             2 -> {
                 writeAt(addr, a * b)
+                ip += 4
+            }
+
+            3 -> {
+                writeAt(readAt(1), input)
                 ip += 2
             }
 
-            3 -> writeAt(readAt(1), input)
-            4 -> output = a
-            5 -> if (a != 0) ip = b
-            6 -> if (a == 0) ip = b
+            4 -> {
+                output = a
+                ip += 2
+            }
+
+            5 -> if (a != 0) ip = b else ip += 3
+            6 -> if (a == 0) ip = b else ip += 3
+            7 -> {
+                writeAt(addr, (a < b).compareTo(false))
+                ip += 4
+            }
+
+            8 -> {
+                writeAt(addr, (a == b).compareTo(false))
+                ip += 4
+            }
+
             else -> break
         }
-        ip += 2
     }
     return output
 }
@@ -50,7 +67,7 @@ private fun sunnyChanceAsteroids(data: List<Int>): Any {
 private data class Instruction(val op: Int, val flags: List<Boolean>) {
     companion object {
         fun of(s: Int) = s.toString().padStart(5, '0').let { ss ->
-            Instruction(ss.drop(3).toInt(), listOf(ss[2], ss[1], ss[0]).map { it == '1' })
+            Instruction(ss.drop(3).toInt(), ss.dropLast(2).reversed().map { it == '1' })
         }
     }
 }
