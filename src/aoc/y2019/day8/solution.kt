4b5d0299e73d60b4dd77566b47f4f83203e2159d
spyroid <soyroid@gmail.com>
2024-01-27 22:33:40 +0100

    .

--- src/aoc/y2019/day8/solution.kt
+++ src/aoc/y2019/day8/solution.kt
@@ -7,14 +7,14 @@ private fun main() {
 }
 
 private fun spaceImageFormat(input: String): Any {
-    val (w, h, iSize) = Triple(25, 6, 25 * 6)
+    val (w, iSize) = 25 to 25 * 6
     val part1 = input.windowed(iSize, iSize).map { s -> s.count { it == '0' } to s }.minBy { it.first }.let { p ->
         p.second.count { it == '1' } * p.second.count { it == '2' }
     }
 
-    fun layer(idx: Int) = generateSequence(idx) { it + iSize }.takeWhile { it in input.indices }.map { input[it] }.joinToString("")
-
-    (0..<iSize).map { layer(it) }
+    (0..<iSize).map { idx ->
+        generateSequence(idx) { it + iSize }.take(input.length / iSize).map { input[it] }.joinToString("")
+    }
         .map { s -> s.first { it != '2' }.let { if (it == '1') it else ' ' } }
         .windowed(w, w)
         .onEach { println(it.joinToString("")) }
