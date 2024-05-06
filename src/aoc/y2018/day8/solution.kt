abc353f5b22e360e88a422edcb41d3f166ae1f5e
spyroid <soyroid@gmail.com>
2024-05-06 11:26:57 +0200

    .

--- src/aoc/y2018/day8/solution.kt
+++ src/aoc/y2018/day8/solution.kt
@@ -10,19 +10,15 @@ private fun main() {
 private fun memoryManeuver(nums: List<Int>): Any {
     fun parse(data: List<Int>): Node {
         var payload = data.drop(2)
-        val node = Node()
-        repeat(data.first()) {
-            val n = parse(payload)
-            node.children.add(n)
-            payload = payload.drop(n.len())
+        return Node().apply {
+            children = IntRange(0, data.first() - 1).map { parse(payload).also { payload = payload.drop(it.len()) } }
+            meta = payload.take(data[1])
         }
-        node.meta = payload.take(data[1])
-        return node
     }
     return parse(nums).let { it.metas() to it.value() }
 }
 
-private data class Node(var meta: List<Int> = listOf(), var children: MutableList<Node> = mutableListOf()) {
+private data class Node(var meta: List<Int> = listOf(), var children: List<Node> = listOf()) {
     fun len(): Int = children.sumOf { it.len() } + meta.size + 2
     fun metas(): Int = children.sumOf { it.metas() } + meta.sum()
     fun value(): Int = if (children.isEmpty()) meta.sum() else meta.map { children.getOrNull(it - 1)?.value() ?: 0 }.sum()
