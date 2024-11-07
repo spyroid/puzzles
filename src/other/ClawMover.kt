d17ecdb1365d514601550f42ca784ab005c39d9e
spyroid <soyroid@gmail.com>
2024-11-07 14:45:06 +0100

    .

--- src/other/ClawMover.kt
+++ src/other/ClawMover.kt
@@ -4,7 +4,7 @@ import gears.puzzle
 
 fun main() {
     puzzle {
-        solve(0, listOf(2, 3, 2, 2).toIntArray(), false)
+        solve(0, listOf(2, 3, 2, 2).toIntArray(), true)
     }
 }
 
@@ -12,27 +12,11 @@ private fun solve(clawPos: Int, boxes: IntArray, boxInClaw: Boolean): Any {
     var layout = (boxes.sum() + boxInClaw.compareTo(false)).let { all ->
         boxes.mapIndexed { i, _ -> all / boxes.size + ((all % boxes.size) > i).compareTo(false) }
     }
-
-    println("current: ${boxes.toList()} pos: $clawPos boxInClaw: $boxInClaw")
-    println("planned:  $layout")
-
-    if (boxInClaw) {
-        val target = layout.withIndex().find { (i, v) -> v > boxes[i] }?.index ?: return "ERR"
-//        println("target index: $target")
-        when (target.compareTo(clawPos)) {
-            -1 -> return "LEFT"
-            0 -> return "PLACE"
-            1 -> return "RIGHT"
-        }
-    } else {
-        val target = layout.withIndex().find { (i, v) -> v < boxes[i] }?.index ?: return "ERR"
-//        println("target index: $target")
-        when (target.compareTo(clawPos)) {
-            -1 -> return "LEFT"
-            0 -> return "PICK"
-            1 -> return "RIGHT"
-        }
+    val target = layout.withIndex().find { (i, v) -> (v < boxes[i]) xor boxInClaw }?.index ?: return "ERR"
+    return when (target.compareTo(clawPos)) {
+        -1 -> "LEFT"
+        0 -> if (boxInClaw) "PLACE" else "PICK"
+        1 -> "RIGHT"
+        else -> "NOTHING"
     }
-
-    return "NOTHING"
 }
