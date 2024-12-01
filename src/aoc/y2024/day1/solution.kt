1ce1b9007ce3a9b1399b40b2fd2eaa6eef1173c9
spyroid <soyroid@gmail.com>
2024-12-01 19:37:25 +0100

    .

--- src/aoc/y2024/day1/solution.kt
+++ src/aoc/y2024/day1/solution.kt
@@ -10,12 +10,10 @@ fun main() {
 }
 
 private fun HistorianHysteria(input: List<String>): Any {
-    val a1 = mutableListOf<Int>()
-    val a2 = mutableListOf<Int>()
-    input.map { it.split("\\s+".toRegex()).map(String::toInt) }.forEach { (a, b) -> a1.add(a); a2.add(b) }
+    val (a1, a2) = input.map { it.split("\\s+".toRegex()).map(String::toInt) }.map { (a, b) -> a to b }.unzip()
     val p1 = a1.sorted().zip(a2.sorted()).sumOf { (a, b) -> abs(a - b) }
     val map = a2.groupingBy { it }.eachCount()
-    val p2 = a1.sumOf { map.getOrElse(it) { 0 } * it }
+    val p2 = a1.sumOf { (map[it] ?: 0) * it }
 
     return p1 to p2
 }
