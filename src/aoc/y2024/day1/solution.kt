88270b60c8d5137c3d99b52aa141241eda4a3885
spyroid <soyroid@gmail.com>
2024-12-01 10:23:16 +0100

    .

---
+++ src/aoc/y2024/day1/solution.kt
@@ -0,0 +1,21 @@
+package aoc.y2024.day1
+
+import gears.puzzle
+import kotlin.math.abs
+
+fun main() {
+    puzzle {
+        solve(inputLines())
+    }
+}
+
+private fun solve(input: List<String>): Any {
+    val a1 = mutableListOf<Int>()
+    val a2 = mutableListOf<Int>()
+    input.map { it.split("\\s+".toRegex()).map(String::toInt) }.forEach { (a, b) -> a1.add(a); a2.add(b) }
+    val p1 = a1.sorted().zip(a2.sorted()).sumOf { (a, b) -> abs(a - b) }
+    val map = a2.groupingBy { it }.eachCount()
+    val p2 = a1.sumOf { map.getOrElse(it) { 0 } * it }
+
+    return p1 to p2
+}
