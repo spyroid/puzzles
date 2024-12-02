403cd2dc2b2cc73e768f7e78da99e7b3ae584837
spyroid <soyroid@gmail.com>
2024-12-02 20:03:58 +0100

    .

--- src/aoc/y2024/day2/solution.kt
+++ src/aoc/y2024/day2/solution.kt
@@ -11,7 +11,7 @@ fun main() {
 
 private fun redNosedReports(input: List<String>): Any {
     fun incOrDec(list: List<Int>) = list.zipWithNext { a, b -> a - b }
-        .let { df -> (df.all { it > 0 } || df.all { it < 0 }) && df.all { abs(it) in 1..3 } }
+        .let { df -> (df.all { it > 0 } || df.all { it < 0 }) && df.all { abs(it) <= 3 } }
 
     val (a, b) = input.map { s -> s.split(" ").map { it.toInt() } }.map { list ->
         incOrDec(list) to list.indices.any { incOrDec(list.filterIndexed { i, _ -> i != it }) }
