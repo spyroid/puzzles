be2617563c1c1784160b34280e8515741d074e9f
spyroid <soyroid@gmail.com>
2024-12-02 22:48:40 +0100

    .

--- src/aoc/y2024/day2/solution.kt
+++ src/aoc/y2024/day2/solution.kt
@@ -12,8 +12,7 @@ private fun redNosedReports(input: List<String>): Any {
     fun incOrDec(list: List<Int>) = list.zipWithNext { a, b -> a - b }
         .let { df -> df.all { it in 1..3 } || df.all { it in -3..-1 } }
 
-    val (a, b) = input.map { s -> s.split(" ").map { it.toInt() } }.map { list ->
+    return input.map { s -> s.split(" ").map { it.toInt() } }.map { list ->
         incOrDec(list) to list.indices.any { incOrDec(list.filterIndexed { i, _ -> i != it }) }
-    }.unzip()
-    return a.count { it } to b.count { it }
+    }.unzip().let { (a, b) -> a.count { it } to b.count { it } }
 }
