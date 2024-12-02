b5cb8bad340af2e68f734b53cdeec97d34c3d5b7
spyroid <soyroid@gmail.com>
2024-12-02 20:20:17 +0100

    .

--- src/aoc/y2024/day2/solution.kt
+++ src/aoc/y2024/day2/solution.kt
@@ -1,7 +1,6 @@
 package aoc.y2024.day2
 
 import gears.puzzle
-import kotlin.math.abs
 
 fun main() {
     puzzle {
@@ -11,7 +10,7 @@ fun main() {
 
 private fun redNosedReports(input: List<String>): Any {
     fun incOrDec(list: List<Int>) = list.zipWithNext { a, b -> a - b }
-        .let { df -> (df.all { it > 0 } || df.all { it < 0 }) && df.all { abs(it) <= 3 } }
+        .let { df -> df.all { it in 1..3 } || df.all { it in -3..-1 } }
 
     val (a, b) = input.map { s -> s.split(" ").map { it.toInt() } }.map { list ->
         incOrDec(list) to list.indices.any { incOrDec(list.filterIndexed { i, _ -> i != it }) }
---
+++ src/aoc/y2024/day3/solution.kt
@@ -0,0 +1,13 @@
+package aoc.y2024.day3
+
+import gears.puzzle
+
+fun main() {
+    puzzle {
+        xxx(inputLines())
+    }
+}
+
+private fun xxx(input: List<String>): Any {
+    return 0
+}
