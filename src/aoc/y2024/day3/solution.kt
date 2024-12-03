0eb000857f447dd27010b2cefc3a5700bb2c63a5
spyroid <soyroid@gmail.com>
2024-12-03 07:35:27 +0100

    .

--- src/aoc/y2024/day3/solution.kt
+++ src/aoc/y2024/day3/solution.kt
@@ -7,18 +7,16 @@ fun main() {
 }
 
 private fun mullItOver(input: String): Any {
-    fun search(re: Regex) = re.findAll(input).map { it.range.first to it.groupValues.first() }
-    val re = Regex("""\d+""")
-    fun calc(seq: Sequence<Pair<Int, String>>) = seq.sortedBy { it.first }.map { it.second }
+    val re1 = Regex("""\d+""")
+    fun calc(re: String) = re.toRegex().findAll(input).map { it.groupValues.first() }
         .fold(true to 0) { acc, e ->
             when {
                 e.contains("do(") -> true to acc.second
                 e.contains("don") || !acc.first -> false to acc.second
-                else -> true to acc.second + re.findAll(e).fold(1) { acc, v -> acc * v.value.toInt() }
+                else -> true to acc.second + re1.findAll(e).fold(1) { acc, v -> acc * v.value.toInt() }
             }
         }.second
 
-    val part1 = search(Regex("""mul\(\d+,\d+\)"""))
-    val part2 = part1.plus(search(Regex("""do\(\)|don't\(\)""")))
-    return calc(part1) to calc(part2)
+    val part1 = """mul\(\d+,\d+\)"""
+    return calc(part1) to calc("""${part1}|do\(\)|don't\(\)""")
 }
