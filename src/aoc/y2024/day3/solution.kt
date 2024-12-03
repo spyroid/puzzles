25e747b00ad7e9d2fa6f0713aa534a368856a4f6
spyroid <soyroid@gmail.com>
2024-12-03 07:11:40 +0100

    .

--- src/aoc/y2024/day3/solution.kt
+++ src/aoc/y2024/day3/solution.kt
@@ -3,11 +3,23 @@ package aoc.y2024.day3
 import gears.puzzle
 
 fun main() {
-    puzzle {
-        xxx(inputLines())
-    }
+    puzzle { mullItOver(input()) }
 }
 
-private fun xxx(input: List<String>): Any {
-    return 0
+private fun mullItOver(input: String): Any {
+    fun search(re: Regex) = re.findAll(input).map { it.range.first to it.groupValues.first() }
+    val re = Regex("""\d+""")
+    fun calc(seq: Sequence<Pair<Int, String>>) = seq.sortedBy { it.first }
+        .map { it.second }
+        .fold(Pair(true, 0)) { acc, e ->
+            when {
+                e.contains("do(") -> true to acc.second
+                e.contains("don") || !acc.first -> false to acc.second
+                else -> true to acc.second + re.findAll(e).fold(1) { acc, v -> acc * v.value.toInt() }
+            }
+        }.second
+
+    val part1 = search(Regex("""mul\(\d+,\d+\)"""))
+    val part2 = part1.plus(search(Regex("""do\(\)"""))).plus(search(Regex("""don't\(\)""")))
+    return calc(part1) to calc(part2)
 }
