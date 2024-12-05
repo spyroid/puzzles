301f66a2791af945eb9e50865242a4dbbc310103
spyroid <soyroid@gmail.com>
2024-12-05 22:50:17 +0100

    .

--- src/aoc/y2024/day5/solution.kt
+++ src/aoc/y2024/day5/solution.kt
@@ -10,29 +10,26 @@ private fun printQueue(input: String): Any {
     val (map, list) = input.split("\n\n").let { (a, b) ->
         val m = a.lines().map { it.split("|").map { it.toInt() }.let { it.first() to it.last() } }
             .groupBy({ it.first }, { it.second }).mapValues { it.value.toSet() }
-
-        val l = b.lines().map { it.split(",").map { it.toInt() } }
-
+        val l = b.lines().map { it.split(",").map { it.toInt() }.toMutableList() }
         m to l
     }
 
-    fun findInvalid(list: List<Int>) = list.mapIndexed { i, e ->
-        e to list.takeLast(list.size - i - 1).toSet() - (map[e] ?: emptySet())
-    }.firstOrNull { it.second.isNotEmpty() }?.let { it.first to it.second.last() } ?: (0 to 0)
-
-    fun MutableList<Int>.swap(a: Int, b: Int) {
-        this[a] = this[b].also { this[b] = this[a] }
+    fun MutableList<Int>.fix(): Int {
+        var idx = 0
+        while (true) {
+            val a = indices.map { i ->
+                i to takeLast(size - i - 1).toSet() - (map[get(i)] ?: emptySet())
+            }.find { it.second.isNotEmpty() }
+            if (a == null) return idx
+            this[a.first] = this[indexOf(a.second.last())].also { this[indexOf(a.second.last())] = this[a.first] }
+            idx++
+        }
+        return idx
     }
 
     fun MutableList<Int>.mid() = this[size / 2]
 
-    return list.map { line ->
-        generateSequence(line.toMutableList() to findInvalid(line)) { (line, b) ->
-            line.swap(line.indexOf(b.first), line.indexOf(b.second))
-            line to findInvalid(line)
-        }
-            .withIndex()
-            .first { (_, v) -> v.second.second == 0 }
-            .let { (i, v) -> if (i == 0) v.first.mid() to 0 else 0 to v.first.mid() }
+    return list.map { ints ->
+        ints.fix().let { if (it == 0) ints.mid() to 0 else 0 to ints.mid() }
     }.unzip().let { it.first.sum() to it.second.sum() }
 }
