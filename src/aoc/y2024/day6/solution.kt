93dec72cee90205a2a1aa05d27a3b33b926a2637
spyroid <soyroid@gmail.com>
2024-12-06 13:14:36 +0100

    .

--- src/aoc/y2024/day6/solution.kt
+++ src/aoc/y2024/day6/solution.kt
@@ -11,13 +11,13 @@ fun main() {
 
 fun guardGallivant(input: List<String>): Any {
     fun walk(grid: Grid<Char>): Pair<MutableSet<Pair<Point, Direction>>, Int> {
-        var pos = grid.all().first { it.v == '^' }.p.also { grid.set(it, '.') }
+        var pos = grid.all().first { it.v == '^' }.p
         var dir = Direction.DOWN
         var steps = mutableSetOf<Pair<Point, Direction>>(pos to dir)
         while (true) {
             when (grid.at(pos + dir)) {
                 '#' -> dir = dir.turnCcw()
-                '.' -> pos = pos + dir
+                '.', '^' -> pos += dir
                 else -> return steps to 0
             }
             if (!steps.add(pos to dir)) return steps to 1
@@ -25,6 +25,6 @@ fun guardGallivant(input: List<String>): Any {
     }
 
     var p1 = walk(Grid.of(input) { it }).first.unzip().first.toSet()
-    val p2 = p1.drop(1).sumOf { p -> Grid.of(input) { it }.also { it.set(p, '#') }.let { walk(it).second } }
+    val p2 = p1.drop(1).sumOf { p -> Grid.of(input) { it }.also { it[p] = '#' }.let { walk(it).second } }
     return p1.size to p2
 }
