5cc431b49ea8294a7ed907da13c68427e7032b49
spyroid <soyroid@gmail.com>
2024-12-08 16:19:37 +0100

    .

--- src/aoc/y2024/day8/solution.kt
+++ src/aoc/y2024/day8/solution.kt
@@ -10,21 +10,18 @@ fun main() {
 
 private fun resonantCollinearity(input: List<String>): Any {
     val grid = Grid.of(input) { if (it == '#') '.' else it }
-    fun Point.nextInLine(other: Point, limit: Int) = generateSequence(other to Point(other.x - this.x, other.y - this.y)) { (a, b) -> a + b to b }
+    fun Point.nextInLine(other: Point, limit: Int) = generateSequence(other to Point(other.x - x, other.y - y)) { (a, b) -> a + b to b }
         .withIndex()
         .takeWhile { (i, v) -> i <= limit && grid.at(v.first) != null }
         .drop(1)
         .map { it.value.first }
 
-    val map = grid.all().filter { !".".contains(it.v) }.groupBy({ it.v }, { it.p })
+    val map = grid.all().filter { it.v != '.' }.groupBy({ it.v }, { it.p })
 
     fun antidotes(limit: Int = Int.MAX_VALUE) = sequence {
         map.values.forEach { list ->
             list.dropLast(1).forEachIndexed { i, a ->
-                list.drop(i + 1).forEach { b ->
-                    yieldAll(a.nextInLine(b, limit))
-                    yieldAll(b.nextInLine(a, limit))
-                }
+                list.drop(i + 1).forEach { b -> yieldAll(a.nextInLine(b, limit) + b.nextInLine(a, limit)) }
             }
         }
     }
