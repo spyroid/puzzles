7ba2335948971a9344b7c7aa0a6faf6a2c5130b3
spyroid <soyroid@gmail.com>
2024-12-10 11:43:39 +0100

    .

--- src/aoc/y2024/day10/solution.kt
+++ src/aoc/y2024/day10/solution.kt
@@ -8,21 +8,16 @@ fun main() {
 }
 
 private fun hoofIt(input: List<String>): Any {
-    val grid = Grid.of(input) { if (it == '.') -1 else it.digitToInt() }
-
-    val (p1, p2) = grid.all().filter { it.v == 0 }.map { t ->
+    val grid = Grid.of(input) { it.digitToInt() }
+    return grid.all().filter { it.v == 0 }.map { t ->
         val seen = mutableSetOf<Grid.GValue<Int>>()
-        var cross = 0
         val track = ArrayDeque<Grid.GValue<Int>>().apply { add(t) }
+        var count = 0
         while (track.isNotEmpty()) {
-            val current = track.removeFirst()
-            seen.add(current)
-            val around = grid.gvAround(current.p).filter { it.v == current.v + 1 }
-            if (around.isEmpty() && current.v == 9) cross++
-            track.addAll(around)
+            val current = track.removeFirst().also { seen.add(it) }
+            val around = grid.gvAround(current.p).filter { it.v == current.v + 1 }.also { track.addAll(it) }
+            if (around.isEmpty() && current.v == 9) count++
         }
-        seen.count { it.v == 9 } to cross
-    }.unzip().let{ (a, b) -> a.sum() to b.sum() }
-
-    return p1 to p2
+        seen.count { it.v == 9 } to count
+    }.unzip().let { (a, b) -> a.sum() to b.sum() }
 }
