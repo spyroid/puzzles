5af7b0ebc768ccb8996c9e9ba49dbc3fa33e047b
spyroid <soyroid@gmail.com>
2024-12-13 17:55:48 +0100

    .

--- src/aoc/y2024/day13/solution.kt
+++ src/aoc/y2024/day13/solution.kt
@@ -23,10 +23,8 @@ private fun clawContraption(input: List<String>): Any {
 
         fun solve(ax: Double, bx: Double, ay: Double, by: Double, px: Double, py: Double): Long {
             val det = (ax * by - ay * bx)
-
             val x = (px * by - py * bx) / det
             val y = (py * ax - px * ay) / det
-
             return if (x % 1.0 == 0.0 && y % 1.0 == 0.0) x.toLong() * 3 + y.toLong() else 0L
         }
 
