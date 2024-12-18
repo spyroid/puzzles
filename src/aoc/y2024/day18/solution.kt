de46edd6d6aaab95bd010a3fd54f5050967a9a95
spyroid <soyroid@gmail.com>
2024-12-18 22:18:07 +0100

    .

---
+++ src/aoc/y2024/day18/solution.kt
@@ -0,0 +1,43 @@
+package aoc.y2024.day18
+
+import gears.Grid
+import gears.Point
+import gears.puzzle
+
+fun main() {
+    puzzle { RAMRun(inputLines()) }
+}
+
+private fun RAMRun(input: List<String>): Any {
+
+    val allWalls = input.map { it.split(",").let { (a, b) -> Point(a.toInt(), b.toInt()) } }
+    val w = 70;
+    val h = 70
+
+    fun bfs(walls: Set<Point>, start: Point, end: Point): Int {
+        val queue = ArrayDeque<Grid.Entry<Int>>().apply { add(Grid.Entry(start, 0)) }
+        val visited = mutableSetOf<Point>()
+        while (queue.isNotEmpty()) {
+            val current = queue.removeFirst()
+            if (current.p == end) return current.v
+            current.p.around4()
+                .filter { it.withinBounds(end) && !visited.contains(it) && !walls.contains(it) }
+                .forEach {
+                    queue.add(Grid.Entry(it, current.v + 1))
+                    visited.add(it)
+                }
+        }
+        return -1
+    }
+
+    val part1 = bfs(allWalls.take(1024).toSet(), Point.zero, Point(w, h))
+
+    val currentWalls = mutableSetOf<Point>()
+    for (wall in allWalls) {
+        currentWalls.add(wall)
+        if (bfs(currentWalls, Point.zero, Point(w, h)) == -1) break
+    }
+
+    return part1 to currentWalls.last()
+}
+
