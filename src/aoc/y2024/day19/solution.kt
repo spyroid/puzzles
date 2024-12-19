bac8245f1e85d87e5b1f96ad17dd420d73b4b2a4
spyroid <soyroid@gmail.com>
2024-12-19 16:30:32 +0100

    .

---
+++ src/aoc/y2024/day19/solution.kt
@@ -0,0 +1,21 @@
+package aoc.y2024.day19
+
+import gears.puzzle
+
+fun main() {
+    puzzle { linenLayout(inputLines()) }
+}
+
+private fun linenLayout(input: List<String>): Any {
+    val (towels, designs) = input.first().split(", ") to input.drop(2)
+    val cache = mutableMapOf<String, Long>()
+    fun count(design: String): Long {
+        if (design.isEmpty()) return 1
+        cache[design]?.let { return it }
+        return towels.sumOf { if (design.startsWith(it)) count(design.removePrefix(it)) else 0 }
+            .also { cache[design] = it }
+    }
+
+    val possibilities = designs.map { count(it) }
+    return possibilities.count { it > 0 } to possibilities.sumOf { it }
+}
