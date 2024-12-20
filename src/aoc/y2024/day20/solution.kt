ffbcc8ab501bee44dbb8c904f53b88a52ffbb8e1
spyroid <soyroid@gmail.com>
2024-12-20 16:45:41 +0100

    .

--- src/aoc/y2024/day20/solution.kt
+++ src/aoc/y2024/day20/solution.kt
@@ -19,7 +19,7 @@ private fun raceCondition(input: List<String>): Any {
     var start = grid.all().first { it.v == 'S' }.p
     var end = grid.all().first { it.v == 'E' }.p
 
-    fun bfs(s: Point, e: Point): Pair<MutableMap<Point, Int>, Int> {
+    fun find(s: Point, e: Point): Pair<MutableMap<Point, Int>, Int> {
         val pq = ArrayDeque<Grid.Entry<Int>>().apply { add(Grid.Entry(s, 0)) }
         val d = mutableMapOf<Point, Int>()
         var best = -1
@@ -28,24 +28,21 @@ private fun raceCondition(input: List<String>): Any {
             val (point, score) = pq.removeFirst()
             if (d.containsKey(point)) continue
             d[point] = score
-            if (point == end) best = score
+            if (point == e) best = score
             grid.around4(point).filter { it.v != '#' }.forEach { pq.addFirst(Grid.Entry(it.p, score + 1)) }
         }
         return d to best
     }
 
-    val (distStart, score0) = bfs(start, end) // start, end
-    val (distEnd, _) = bfs(end, start)
+    val (distStart, score0) = find(start, end) // start, end
+    val (distEnd, _) = find(end, start)
 
     fun cheats(threshold: Int, saveSeconds: Int): Int {
         var res = 0
 
-        for (i in 0..grid.maxY()) for (j in 0..grid.maxX()) {
-            val pos = Point(j, i)
-            if (grid[pos] == '#' || pos !in distStart) continue
-
-            for (k in range(max(i - threshold, 0), min(i + threshold + 1, grid.maxY())))
-                for (l in range(max(j - threshold, 0), min(j + threshold + 1, grid.maxX()))) {
+        grid.all().filter { it.v != '#' && it.p in distStart }.forEach { (pos, _) ->
+            for (k in range(max(pos.y - threshold, 0), min(pos.y + threshold + 1, grid.maxY())))
+                for (l in range(max(pos.x - threshold, 0), min(pos.x + threshold + 1, grid.maxX()))) {
                     val p = Point(l, k)
                     var dist = pos.manhattan(p)
                     if (dist > threshold || grid[p] == '#' || p !in distEnd.keys) continue
