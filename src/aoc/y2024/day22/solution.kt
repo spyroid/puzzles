e8bbfc656c045a9ebfeb359bd0926293e40dfbd9
spyroid <soyroid@gmail.com>
2024-12-22 17:20:15 +0100

    .

--- src/aoc/y2024/day22/solution.kt
+++ src/aoc/y2024/day22/solution.kt
@@ -9,7 +9,7 @@ fun main() {
 private fun monkeyMarket(input: List<String>): Any {
 
     fun mix(a: Long, b: Long) = (a xor b) % 16777216L
-    fun next(input: Long): Long = mix(input * 64, input).let { mix(it / 32, it) }.let { mix(it * 2048, it) }
+    fun next(input: Long) = mix(input * 64, input).let { mix(it / 32, it) }.let { mix(it * 2048, it) }
 
     val part1 = input.sumOf { generateSequence(next(it.toLong())) { next(it) }.take(2000).last() }
 
@@ -29,4 +29,3 @@ private fun monkeyMarket(input: List<String>): Any {
 
     return part1 to part2
 }
-
