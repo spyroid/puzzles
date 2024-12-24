f9cd11242008e856c80895601df183888fe0ab67
spyroid <soyroid@gmail.com>
2024-12-24 22:22:02 +0100

    .

--- src/aoc/y2024/day24/solution.kt
+++ src/aoc/y2024/day24/solution.kt
@@ -8,22 +8,22 @@ fun main() {
 
 private fun crossedWires(input: String): Any {
     val (regs, commands) = input.split("\n\n").let { (a, b) ->
-        a.lines().map { it.split(": ") }.associate { (x, y) -> x to y.toInt() }.toMutableMap() to
-                b.lines().map { it.split(" ") }
+        a.lines().map { it.split(": ") }.associate { (x, y) -> x to y.toInt() }.toMutableMap() to b.lines().map { it.split(" ") }
+    }
+
+    fun exec(x: Int, y: Int, op: String, z: String) = when (op) {
+        "XOR" -> regs[z] = x xor y
+        "OR" -> regs[z] = x or y
+        else -> regs[z] = x and y
     }
 
     val set = mutableSetOf<Int>()
     while (set.size < commands.size) {
         commands.forEachIndexed { i, line ->
-            val (a, op, b, _, target) = line
-            val aa = regs[a]
-            val bb = regs[b]
-            if (aa == null || bb == null || !set.add(i)) return@forEachIndexed
-            when (op) {
-                "XOR" -> regs[target] = aa xor bb
-                "OR" -> regs[target] = aa or bb
-                "AND" -> regs[target] = aa and bb
-            }
+            val (x, op, y, _, z) = line
+            val (xx, yy) = regs[x] to regs[y]
+            if (xx == null || yy == null || !set.add(i)) return@forEachIndexed
+            exec(xx, yy, op, z)
         }
     }
 
@@ -33,5 +33,24 @@ private fun crossedWires(input: String): Any {
         .joinToString("")
         .toLong(2)
 
-    return part1
+    // @lscddit
+    val lastZ = commands.map { it.last() }.maxOf { it }
+    val wrong = mutableSetOf<String>()
+
+    commands.forEach { (x, op, y, _, z) ->
+        if (z.first() == 'z' && op != "XOR" && z != lastZ) wrong.add(z)
+        if (op == "XOR" && listOf(x, y, z).all { it.first() !in "zyx" }) wrong.add(z)
+
+        if (op == "AND" && x != "x00" && y != "x00") {
+            commands.forEach { (xx, op2, yy) -> if ((z == xx || z == yy) && op2 != "OR") wrong.add(z) }
+        }
+
+        if (op == "XOR") {
+            commands.forEach { (xx, op2, yy) -> if ((z == xx || z == yy) && op2 == "OR") wrong.add(z) }
+        }
+
+    }
+    val part2 = wrong.sorted().joinToString(",")
+
+    return part1 to part2
 }
