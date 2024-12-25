273e31504da05f9a8e283e3e8ef40043b2dff6ed
spyroid <soyroid@gmail.com>
2024-12-25 13:25:37 +0100

    .

--- src/aoc/y2024/day25/solution.kt
+++ src/aoc/y2024/day25/solution.kt
@@ -7,18 +7,14 @@ fun main() {
 }
 
 private fun codeChronicle(input: String): Any {
-    val blocks = input.replace('#', '1').replace('.', '0').split("\n\n")
-
-    val (locks, keys) = blocks.map { block ->
-        val a = block.lines().map { s -> s.map { it.digitToInt() } }.reduce { a, b -> a.zip(b).map { it.first + it.second } }
-        a to block.first()
-    }.groupBy { it.second }.mapValues { it.value.map { it.first } }.values.toList()
-
-    val c = locks.map { lock ->
-        keys.map { key ->
-            lock.zip(key).map { (a, b) -> a + b }.all { it <= 7 }
+    return input.replace('#', '1').replace('.', '0').split("\n\n")
+        .map { block ->
+            val a = block.lines().map { s -> s.map { it.digitToInt() } }.reduce { a, b -> a.zip(b).map { it.first + it.second } }
+            a to block.first()
+        }.groupBy({ it.second }, { it.first }).values.toList()
+        .let { (locks, keys) ->
+            locks.map { lock ->
+                keys.map { key -> lock.zip(key).map { (a, b) -> a + b }.all { it <= 7 } }
+            }.flatten().count { it }
         }
-    }.flatten().count { it }
-
-    return c
 }
