1ffc135bc8373581e6bfc815336387e83ec04323
spyroid <soyroid@gmail.com>
2024-12-25 14:02:29 +0100

    .

--- src/aoc/y2024/day25/solution.kt
+++ src/aoc/y2024/day25/solution.kt
@@ -6,15 +6,12 @@ fun main() {
     puzzle { codeChronicle(input()) }
 }
 
-private fun codeChronicle(input: String): Any {
-    return input.replace('#', '1').replace('.', '0').split("\n\n")
-        .map { block ->
-            val a = block.lines().map { s -> s.map { it.digitToInt() } }.reduce { a, b -> a.zip(b).map { it.first + it.second } }
-            a to block.first()
-        }.groupBy({ it.second }, { it.first }).values.toList()
-        .let { (locks, keys) ->
-            locks.map { lock ->
-                keys.map { key -> lock.zip(key).map { (a, b) -> a + b }.all { it <= 7 } }
-            }.flatten().count { it }
-        }
-}
+private fun codeChronicle(input: String) = input.split("\n\n").map { block ->
+    block.lines().map { s -> s.map { if (it == '#') 1 else 0 } }
+        .reduce { a, b -> a.zip(b).map { it.first + it.second } } to block.first()
+}.groupBy({ it.second }, { it.first }).values.toList()
+    .let { (locks, keys) ->
+        locks.map { lock ->
+            keys.map { key -> lock.zip(key).map { (a, b) -> a + b }.all { it <= 7 } }
+        }.flatten().count { it }
+    }
