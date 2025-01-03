4b7cff5224924ffd596d273ef70a2437f05bc958
spyroid <soyroid@gmail.com>
2025-01-03 22:44:29 +0100

    .

--- src/aoc/y2018/day6/solution.kt
+++ src/aoc/y2018/day6/solution.kt
@@ -2,7 +2,6 @@ package aoc.y2018.day6
 
 import gears.Point
 import gears.bounds
-import gears.inc
 import gears.puzzle
 
 fun main() {
@@ -10,19 +9,17 @@ fun main() {
 }
 
 private fun `Chronal Coordinates`(input: List<String>): Any {
-    val centroid = input.map { Point.of(it, ", ") }.toSet()
-    val (p1, p2) = centroid.bounds()
+    val centroids = input.map { Point.of(it, ", ") }.toSet()
+    val (p1, p2) = centroids.bounds()
 
     val unboundedCentroids = mutableSetOf<Point>()
-    val centroidAreas = mutableMapOf<Point, Int>()
-
-    p1.allInBounds(p2).forEach { p ->
-        val (a, b) = centroid.map { it.manhattan(p) to it }.sortedBy { it.first }
-        if (a.first != b.first) centroidAreas.inc(a.second)
+    val part1 = p1.allInBounds(p2).mapNotNull { p ->
+        val (a, b) = centroids.map { it.manhattan(p) to it }.sortedBy { it.first }
         if (p.x == p1.x || p.y == p1.y || p.x == p2.x || p.y == p2.y) unboundedCentroids.add(a.second)
-    }
-    val part1 = centroidAreas.filter { it.key !in unboundedCentroids }.maxOf { it.value }
-    val part2 = p1.allInBounds(p2).sumOf { p -> if (centroid.sumOf { it.manhattan(p) } < 10_000) 1 else 0.toInt() }
+        if (a.first != b.first) a.second else null
+    }.groupingBy { it }.eachCount().filter { it.key !in unboundedCentroids }.maxOf { it.value }
+
+    val part2 = p1.allInBounds(p2).sumOf { p -> if (centroids.sumOf { it.manhattan(p) } < 10_000) 1 else 0.toInt() }
 
     return part1 to part2
 }
