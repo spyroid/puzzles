d33f593da8c1ace910c9ef1a6b7e4c3ba1fc21ce
spyroid <soyroid@gmail.com>
2025-01-07 14:03:28 +0100

    .

--- src/aoc/y2020/day20/solution.kt
+++ src/aoc/y2020/day20/solution.kt
@@ -1,29 +1,125 @@
 package aoc.y2020.day20
 
-import gears.Grid2
-import gears.findInts
+import gears.Grid
+import gears.Point
+import gears.findLongs
 import gears.puzzle
+import kotlin.math.sqrt
 
 fun main() {
-    puzzle { `Jurassic Jigsaw`(input("test.txt")) }
-//    puzzle { `Jurassic Jigsaw`(input()) }
+    puzzle { `Jurassic Jigsaw`(input()) }
 }
 
 private fun `Jurassic Jigsaw`(input: String): Any {
     val tiles = input.split("\n\n").map { block ->
-        block.lines().let { lines -> Tile(lines.first().findInts().first(), Grid2.of(lines.drop(1)) { if (it == '.') 0 else 1 }) }
+        block.lines().let { lines -> Tile(lines.first().findLongs().first(), Grid.of(lines.drop(1)) { it }) }
     }
 
-    fun find(tile: Tile) = tiles.filter { it.id != tile.id }.mapNotNull { if (it.allEdges.intersect(tile.allEdges).isNotEmpty()) it else null }
+    val image = createImage(tiles)
 
-    val found = tiles.map { Pair(it.id, find(it)) }.onEach { println("${it.first} -> ${it.second.map { it.id }}") }
+    val part1 = image.first().first().id * image.first().last().id * image.last().first().id * image.last().last().id
 
-    val part1 = found.filter { it.second.size == 2 }.map { it.first }.fold(1L) { v, acc -> acc * v }
+    val seaMonster = listOf(
+        Point(0, 18), Point(1, 0), Point(1, 5), Point(1, 6), Point(1, 11), Point(1, 12),
+        Point(1, 17), Point(1, 18), Point(1, 19), Point(2, 1), Point(2, 4), Point(2, 7),
+        Point(2, 10), Point(2, 13), Point(2, 16)
+    )
 
-    return part1
+    val part2 = imageToSingleTile(image).orientations().first { it.maskIfFound(seaMonster) }.grid.all().count { it.v == '#' }
+
+    return part1 to part2
 }
 
-private data class Tile(val id: Int, var grid: Grid2<Int>) {
-    val edges = listOf(grid.topEdge(), grid.bottomEdge(), grid.leftEdge(), grid.rightEdge())
-    val allEdges = edges.plus(edges.map { it.reversed() }).map { list -> list.map { it.digitToChar() }.joinToString("").toInt(2) }.toSet()
+private fun imageToSingleTile(image: List<List<Tile>>): Tile {
+    val lines = buildList {
+        image.map { rowTiles ->
+            for (r in 1..rowTiles.first().grid.maxY - 1) {
+                val line = rowTiles.joinToString("") { rowTile -> rowTile.grid.data[r].drop(1).dropLast(1).map { it.v }.joinToString("") }
+                add(line)
+            }
+        }
+    }
+    return Tile(0, Grid.of(lines) { it })
+}
+
+private fun createImage(tiles: List<Tile>): List<List<Tile>> {
+    val width = sqrt(tiles.count().toFloat()).toInt()
+    var mostRecentTile = findTopCorner(tiles)
+    var mostRecentRowHeader = mostRecentTile
+    return (0 until width).map { row ->
+        (0 until width).map { col ->
+            when {
+                row == 0 && col == 0 -> mostRecentTile
+
+                col == 0 -> {
+                    mostRecentRowHeader = mostRecentRowHeader.findAndOrientNeighbor(Orientation.South, Orientation.North, tiles)
+                    mostRecentTile = mostRecentRowHeader
+                    mostRecentRowHeader
+                }
+
+                else -> {
+                    mostRecentTile = mostRecentTile.findAndOrientNeighbor(Orientation.East, Orientation.West, tiles)
+                    mostRecentTile
+                }
+            }
+        }
+    }
+}
+
+private fun findTopCorner(tiles: List<Tile>) =
+    tiles
+        .first { it.sharedSideCount(tiles) == 2 }
+        .orientations()
+        .first { it.isSideShared(Orientation.South, tiles) && it.isSideShared(Orientation.East, tiles) }
+
+private data class Tile(val id: Long, var grid: Grid<Char>) {
+    private val sides = Orientation.entries.map { sideFacing(it) }.toSet()
+    private val sidesReversed = sides.map { it.reversed() }.toSet()
+    private fun sideFacing(dir: Orientation) =
+        when (dir) {
+            Orientation.North -> grid.data.first().map { it.v }.joinToString("")
+            Orientation.South -> grid.data.last().map { it.v }.joinToString("")
+            Orientation.West -> grid.data.map { row -> row.first() }.map { it.v }.joinToString("")
+            Orientation.East -> grid.data.map { row -> row.last() }.map { it.v }.joinToString("")
+        }
+
+    fun orientations(): Sequence<Tile> = sequence {
+        repeat(2) {
+            repeat(4) {
+                yield(this@Tile.rotate())
+            }
+            this@Tile.flip()
+        }
+    }
+
+    fun rotate() = grid.rotateClockwise().let { grid = it; this }
+    fun flip() = grid.flipX().let { grid = it; this }
+    private fun hasSide(side: String) = side in sides || side in sidesReversed
+    fun sharedSideCount(tiles: List<Tile>) = sides.sumOf { side -> tiles.filterNot { it.id == id }.count { tile -> tile.hasSide(side) } }
+    fun isSideShared(dir: Orientation, tiles: List<Tile>) = tiles.filterNot { it.id == id }.any { tile -> tile.hasSide(sideFacing(dir)) }
+    fun findAndOrientNeighbor(mySide: Orientation, theirSide: Orientation, tiles: List<Tile>): Tile {
+        val mySideValue = sideFacing(mySide)
+        return tiles.filterNot { it.id == id }.first { it.hasSide(mySideValue) }.also { it.orientToSide(mySideValue, theirSide) }
+    }
+    private fun orientToSide(side: String, direction: Orientation) = orientations().first { it.sideFacing(direction) == side }
+    fun maskIfFound(mask: List<Point>): Boolean {
+        var found = false
+        val maxWidth = mask.maxByOrNull { it.y }!!.y
+        val maxHeight = mask.maxByOrNull { it.x }!!.x
+        (0..(grid.height - maxHeight)).forEach { x ->
+            (0..(grid.width - maxWidth)).forEach { y ->
+                val lookingAt = Point(x, y)
+                val actualSpots = mask.map { it + lookingAt }
+                if (actualSpots.all { grid[it]!!.v == '#' }) {
+                    found = true
+                    actualSpots.forEach { grid[it] = '0' }
+                }
+            }
+        }
+        return found
+    }
+}
+
+private enum class Orientation {
+    North, East, South, West
 }
--- src/gears/Grid.kt
+++ src/gears/Grid.kt
@@ -1,16 +1,19 @@
 package gears
 
-class Grid<T>(val data: Array<Array<Entry<T>>>, val width: Int, val height: Int) {
+class Grid<T>(val data: Array<Array<Entry<T>>>) {
     data class Entry<T>(val p: Point, var v: T)
 
+    val width = data.firstOrNull()?.size ?: 0
+    val height = data.size
     val maxX = width - 1
     val maxY = height - 1
 
     companion object {
         fun <T> of(input: List<String>, mapper: (Char) -> T) = (input.first().length to input.size).let { (w, h) ->
-            Grid(Array(h) { y -> Array(w) { x -> Entry(Point(x, y), mapper(input[y][x])) } }, w, h)
+            Grid(Array(h) { y -> Array(w) { x -> Entry(Point(x, y), mapper(input[y][x])) } })
         }
-        fun <T> of(width: Int, height: Int, v: T) = Grid(Array(height) { y -> Array(width) { x -> Entry(Point(x, y), v) } }, width, height)
+
+        fun <T> of(width: Int, height: Int, v: T) = Grid(Array(height) { y -> Array(width) { x -> Entry(Point(x, y), v) } })
     }
 
     override fun toString() = data.joinToString("\n") { it.map { it.v }.joinToString("") }
@@ -26,8 +29,10 @@ class Grid<T>(val data: Array<Array<Entry<T>>>, val width: Int, val height: Int)
     fun around4(p: Point) = p.around4().mapNotNull { get(it) }
     fun around8(p: Point) = p.around8().mapNotNull { get(it) }
 
+    fun rotateClockwise() = Array(width) { i -> Array(height) { j -> Entry(Point(j, i), this.data[maxY - j][i].v) } }.let { Grid(it) }
+    fun flipX() = Array(height) { y -> Array(width) { x -> Entry(Point(x, y), this.data[maxY - y][x].v) } }.let { Grid(it) }
     fun <R> clone(transformer: Grid<T>.(e: Entry<T>) -> R) = Array(height) { y -> Array(width) { x -> Entry(Point(x, y), transformer(data[y][x])) } }
-        .let { Grid(it, width, height) }
+        .let { Grid(it) }
 
     @Suppress("UNCHECKED_CAST")
     fun inc(p: Point, amount: Int = 1) {
@@ -38,4 +43,5 @@ class Grid<T>(val data: Array<Array<Entry<T>>>, val width: Int, val height: Int)
             is Char -> set(p, v.plus(amount) as T)
         }
     }
+
 }
