46135f16e50e8414956f5685bf0604d57f7be75d
Andrei Kavaleuski <andrei.kavaleuski@oracle.com>
2025-12-01 14:13:42 +0100

    .

--- src/aoc/y2025/day1/solution.kt
+++ src/aoc/y2025/day1/solution.kt
@@ -1,6 +1,7 @@
 package aoc.y2025.day1
 
 import gears.puzzle
+import kotlin.math.absoluteValue
 
 fun main() {
     puzzle {
@@ -9,5 +10,18 @@ fun main() {
 }
 
 private fun secretEntrance(input: List<String>): Any {
-    return 0
+    val numbers = input.map { it.replace("L", "-").replace("R", "").toInt() }
+    val part1 = numbers.runningFold(50) { acc, delta ->
+        (acc + delta).mod(100)
+    }.count { it == 0 }
+
+    val part2 = numbers.runningFold(Pair(50, 0)) { acc, delta ->
+        val corrected = if (delta > 0) acc.first else (100 - acc.first) % 100
+        val zeroPasses = (corrected + delta.absoluteValue) / 100
+        val newValue = (acc.first + delta).mod(100)
+        Pair(newValue, zeroPasses)
+    }
+        .sumOf { it.second }
+
+    return part1 to part2
 }
