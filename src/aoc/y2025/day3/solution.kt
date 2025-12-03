e1d66a5935706bbc9d694b9fb9ff21ba0c24627f
Andrei Kavaleuski <andrei.kavaleuski@oracle.com>
2025-12-03 18:40:42 +0100

    .

---
+++ src/aoc/y2025/day3/solution.kt
@@ -0,0 +1,14 @@
+package aoc.y2025.day3
+
+import gears.puzzle
+import gears.toDigits
+
+fun main() {
+    puzzle {
+        lobby(input())
+    }
+}
+
+private fun lobby(input: String): Any {
+    return 0
+}
