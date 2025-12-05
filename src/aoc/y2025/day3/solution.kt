665d9dbc85fa1c1a99cb3ce1de7fcbd9072a2a48
spyroid <soyroid@gmail.com>
2025-12-05 02:46:14 +0100

    .

--- src/aoc/y2025/day3/solution.kt
+++ src/aoc/y2025/day3/solution.kt
@@ -5,10 +5,17 @@ import gears.toDigits
 
 fun main() {
     puzzle {
-        lobby(input())
+        lobby(inputLines())
     }
 }
 
-private fun lobby(input: String): Any {
-    return 0
+private fun lobby(input: List<String>): Any {
+    fun maxFrom(str: String) = str.dropLast(1).indices.maxBy { str[it] }
+        .let { a ->
+            str.drop(a + 1).maxOf { it }.let { b -> "${str[a]}$b".toInt() }
+        }
+
+    val part1 = input.sumOf { maxFrom(it) }
+
+    return part1
 }
