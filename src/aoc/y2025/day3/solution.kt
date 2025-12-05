83e83787bcd1f208a0c82b2b758f1ff521c31f4b
Andrei Kavaleuski <andrei.kavaleuski@oracle.com>
2025-12-05 13:56:17 +0100

    .

--- src/aoc/y2025/day3/solution.kt
+++ src/aoc/y2025/day3/solution.kt
@@ -12,8 +12,9 @@ private fun lobby(input: List<String>): Any {
     fun maximizer(batteries: String, size: Int) = (0..<size)
         .fold(Pair("", batteries)) { acc, i ->
             acc.second.dropLast(size - i - 1).let { frame ->
-                val idx = frame.indices.maxBy { frame[it] }
-                Pair(acc.first + frame[idx], acc.second.drop(idx + 1))
+                frame.indices.maxBy { frame[it] }.let { idx ->
+                    Pair(acc.first + frame[idx], acc.second.drop(idx + 1))
+                }
             }
         }.first.toLong()
 
