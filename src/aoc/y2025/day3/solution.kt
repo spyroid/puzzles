77541fbfc8b3552724c6c75319e8e85865aa00f6
Andrei Kavaleuski <andrei.kavaleuski@oracle.com>
2025-12-05 13:50:07 +0100

    .

--- src/aoc/y2025/day3/solution.kt
+++ src/aoc/y2025/day3/solution.kt
@@ -9,18 +9,13 @@ fun main() {
 }
 
 private fun lobby(input: List<String>): Any {
-    fun maximizer(batteries: String, size: Int): Long {
-        var numbers = ""
-        var remaining = batteries
-        repeat(size) { i ->
-            remaining.dropLast(size - i - 1).let { frame ->
+    fun maximizer(batteries: String, size: Int) = (0..<size)
+        .fold(Pair("", batteries)) { acc, i ->
+            acc.second.dropLast(size - i - 1).let { frame ->
                 val idx = frame.indices.maxBy { frame[it] }
-                numbers += frame[idx]
-                remaining = remaining.drop(idx + 1)
+                Pair(acc.first + frame[idx], acc.second.drop(idx + 1))
             }
-        }
-        return numbers.toLong()
-    }
+        }.first.toLong()
 
     val part1 = input.sumOf { maximizer(it, 2) }
     val part2 = input.sumOf { maximizer(it, 12) }
