511092df31d8e87845679a72f1d081640e31fd4a
spyroid <soyroid@gmail.com>
2025-12-06 15:36:52 +0100

    .

--- src/aoc/y2025/day6/solution.kt
+++ src/aoc/y2025/day6/solution.kt
@@ -11,22 +11,16 @@ fun main() {
 }
 
 private fun trashCompactor(input: List<String>): Any {
-    val grids = input.dropLast(1).let { strings ->
-        strings.first().indices.map { col ->
-            strings.indices.map { row -> strings[row][col] }.joinToString("")
-        }.splitByElement { it.isBlank() }.map { lines -> Grid.of(lines) { it } }
-    }
+    val grids = input.first().indices.map { col ->
+        input.dropLast(1).indices.map { row -> input[row][col] }.joinToString("")
+    }.splitByElement { it.isBlank() }.map { lines -> Grid.of(lines) { it } }
     val ops = input.last().trim().split("\\s+".toRegex())
 
-    val part1 = grids.withIndex().sumOf { (i, grid) ->
-        grid.rotateClockwise().flipY().rows().map { row -> row.map { it.v }.joinToString("").trim().toLong() }
-            .reduce { a, b -> if (ops[i] == "+") a + b else a * b }
+    fun rowToLong(row: List<Grid.Entry<Char>>) = row.map { it.v }.joinToString("").trim().toLong()
+    fun calculate(a: Long, b: Long, op: String) = if (op == "+") a + b else a * b
+    fun partX(mapper: (Grid<Char>) -> Grid<Char>) = grids.withIndex().sumOf { (i, grid) ->
+        mapper(grid).rows().map { rowToLong(it) }.reduce { a, b -> calculate(a, b, ops[i]) }
     }
 
-    val part2 = grids.withIndex().sumOf { (i, grid) ->
-        grid.rows().map { row -> row.map { it.v }.joinToString("").trim().toLong() }
-            .reduce { a, b -> if (ops[i] == "+") a + b else a * b }
-    }
-
-    return part1 to part2
+    return partX { it.rotateClockwise().flipY() } to partX { it }
 }
