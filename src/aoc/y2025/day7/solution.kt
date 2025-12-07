8ecf19c9166cfb744f3e09e6a1656e8125c4c8cb
spyroid <soyroid@gmail.com>
2025-12-07 17:47:45 +0100

    .

--- src/aoc/y2025/day7/solution.kt
+++ src/aoc/y2025/day7/solution.kt
@@ -1,7 +1,5 @@
 package aoc.y2025.day7
 
-import gears.Direction
-import gears.Grid
 import gears.puzzle
 
 fun main() {
@@ -11,29 +9,18 @@ fun main() {
 }
 
 private fun laboratories(input: List<String>): Any {
-    val grid = Grid.of(input) { it }
-
-    val splitters = grid.rows().dropLast(1).map { entries ->
-        entries.mapNotNull { entry ->
-            when (entry.v) {
-                'S' -> grid[entry.p + Direction.UP] = '|'
-                '|' -> grid[entry.p + Direction.UP]?.let { x ->
-                    when (x.v) {
-                        '.' -> grid[entry.p + Direction.UP] = '|'
-                        '^' -> grid.set(entry.p + Direction.UP_LEFT, '|')
-                            .also { grid[entry.p + Direction.UP_RIGHT] = '|' }
-                            .also { return@mapNotNull x }
-                    }
-                }
-            }
-            null
-        }
-    }.flatten()
+    val cols = mutableSetOf(input.first().indexOf('S'))
+    val part1 = input.filterIndexed { idx, _ -> idx % 2 == 0 }.sumOf { line ->
+        (1..<line.length).filter { it in cols && line[it] == '^' }.onEach { col ->
+            cols.remove(col)
+            cols.add(col + 1)
+            cols.add(col - 1)
+        }.count()
+    }
 
     val counters = LongArray(input.first().length) { 0 }.apply {
         this[input.first().indexOf('S')] = 1
     }
-
     input.filterIndexed { idx, _ -> idx % 2 == 0 }.forEach { line ->
         (1..<line.length).forEach { i ->
             if (line[i] == '^') {
@@ -44,5 +31,5 @@ private fun laboratories(input: List<String>): Any {
         }
     }
 
-    return splitters.count() to counters.sum()
+    return part1 to counters.sum()
 }
