663effa317d1938fc843f187b45a99607db49484
Andrei Kavaleuski <andrei.kavaleuski@oracle.com>
2025-12-11 16:36:37 +0100

    .

--- src/aoc/y2025/day11/solution.kt
+++ src/aoc/y2025/day11/solution.kt
@@ -10,8 +10,15 @@ fun main() {
 
 private fun reactor(input: List<String>): Any {
     val devices = input.map { line -> line.split(": ") }.associate { (a, b) -> a to b.split(" ") }
-    fun find(key: String): Long = if (key == "out") 1 else devices.getValue(key).sumOf { find(it) }
-    val part1 = find("you")
 
-    return part1
+    fun find(start: String, end: String, memo: MutableMap<String, Long> = mutableMapOf()): Long {
+        return if (start == end) 1L else memo.getOrPut(start) {
+            devices[start]?.sumOf { next -> find(next, end, memo) } ?: 0
+        }
+    }
+
+    val part1 = find("you", "out")
+    val part2 = find("svr", "fft") * find("fft", "dac") * find("dac", "out")
+
+    return part1 to part2
 }
